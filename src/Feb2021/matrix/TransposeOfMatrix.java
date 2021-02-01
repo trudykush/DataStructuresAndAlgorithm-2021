48e417d1fcf6ab7c8422981466b040e0895e2dae
trudykush <kushalmishra91@gmail.com>
2021-02-01 22:22:49 +0000

    Testing commit from Intellij

--- src/Feb2021/matrix/TransposeOfMatrix.java
+++ src/Feb2021/matrix/TransposeOfMatrix.java
@@ -1,7 +1,10 @@
 package Feb2021.matrix;
 
+import java.util.Scanner;
+
 public class TransposeOfMatrix {
     public static void main(String[] args) {
-
+        Scanner sc = new Scanner(System.in);
+        int numberOfCases = sc.nextInt();
     }
 }
