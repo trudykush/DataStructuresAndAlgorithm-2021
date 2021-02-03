bba0f43ba7d72847ad05835985e4c6c6a7b900e7
trudykush <kushalmishra91@gmail.com>
2021-02-03 23:03:07 +0000

    Base structure

--- src/Feb2021/matrix/RotateAMatrixBy90Degree.java
+++ src/Feb2021/matrix/RotateAMatrixBy90Degree.java
@@ -1,4 +1,27 @@
 package Feb2021.matrix;
 
+import java.util.Scanner;
+
 public class RotateAMatrixBy90Degree {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int numberOfCases = sc.nextInt();
+        
+        while (numberOfCases-- > 0) {
+            int rowsAndColumn = sc.nextInt();
+            int[][] matrix = new int[rowsAndColumn][rowsAndColumn];
+
+            for (int i = 0; i < rowsAndColumn; i++) {
+                for (int j = 0; j < rowsAndColumn; j++) {
+                    matrix[i][j] = sc.nextInt();
+                }
+            }
+            
+            //rotatingMatrix(matrix);
+            //printingResult(matrix);
+        }
+    }
+
+
 }
