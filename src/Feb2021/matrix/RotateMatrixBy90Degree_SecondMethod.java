948398b3de41d1cf145888bef0193d7104d3e7c9
trudykush <kushalmishra91@gmail.com>
2021-02-03 23:54:34 +0000

    algorithm description

--- src/Feb2021/matrix/RotateMatrixBy90Degree_SecondMethod.java
+++ src/Feb2021/matrix/RotateMatrixBy90Degree_SecondMethod.java
@@ -2,6 +2,23 @@ package Feb2021.matrix;
 
 import java.util.Scanner;
 
+/*
+* Algorithm:
+
+There is N/2 squares or cycles in a matrix of side N.
+ Process a square one at a time.
+ Run a loop to traverse the matrix a cycle at a time,
+ i.e loop from 0 to N/2 – 1, loop counter is i
+Consider elements in group of 4 in current square,
+ rotate the 4 elements at a time.
+ So the number of such groups in a cycle is N – 2*i.
+So run a loop in each cycle from x to N – x – 1, loop counter is y
+The elements in the current group is (x, y), (y, N-1-x), (N-1-x, N-1-y),
+ (N-1-y, x), now rotate the these 4 elements, i.e (x, y) <- (y, N-1-x),
+ (y, N-1-x)<- (N-1-x, N-1-y), (N-1-x, N-1-y)<- (N-1-y, x), (N-1-y, x)<- (x, y)
+Print the matrix.
+* */
+
 public class RotateMatrixBy90Degree_SecondMethod {
 
     public static void main(String[] args) {
@@ -18,11 +35,16 @@ public class RotateMatrixBy90Degree_SecondMethod {
                 }
             }
 
-            rotatingMatrix(matrix);
+            rotatingMatrix(matrix, rowsAndColumn);
         }
     }
 
-    private static void rotatingMatrix(int[][] matrix) {
+    private static void rotatingMatrix(int[][] matrix, int N) {
+        // Consider all squares one by one
+        for (int i = 0; i < N / 2; i++) {
+            for (int j = 0; j < N - i - 1; j++) {
 
+            }
+        }
     }
 }
