13b4f08a91d4ec6ec2894927cb6e8d80de0f7af6
trudykush <kushalmishra91@gmail.com>
2021-02-03 23:50:12 +0000

    base method

--- src/Feb2021/matrix/RotateMatrixBy90Degree_SecondMethod.java
+++ src/Feb2021/matrix/RotateMatrixBy90Degree_SecondMethod.java
@@ -1,4 +1,28 @@
 package Feb2021.matrix;
 
+import java.util.Scanner;
+
 public class RotateMatrixBy90Degree_SecondMethod {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int numberOfCases = sc.nextInt();
+
+        while (numberOfCases-- > 0) {
+            int rowsAndColumn = sc.nextInt();
+            int[][] matrix = new int[rowsAndColumn][rowsAndColumn];
+
+            for (int i = 0; i < rowsAndColumn; i++) {
+                for (int j = 0; j < rowsAndColumn; j++) {
+                    matrix[i][j] = sc.nextInt();
+                }
+            }
+
+            rotatingMatrix(matrix);
+        }
+    }
+
+    private static void rotatingMatrix(int[][] matrix) {
+
+    }
 }
