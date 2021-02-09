64ba909444762040ca9ab267728b1363148532b2
trudykush <kushalmishra91@gmail.com>
2021-02-09 22:41:25 +0000

    InsertionSort - Java

--- src/Feb2021/sorting/java/InsertionSort.java
+++ src/Feb2021/sorting/java/InsertionSort.java
@@ -15,9 +15,21 @@ public class InsertionSort {
                 arr[i] = sc.nextInt();
             }
 
-       //     performingInsertionSort(arr, n);
+            performingInsertionSort(arr, n);
         }
     }
 
+    private static void performingInsertionSort(int[] arr, int n) {
+        for (int i = 1; i < n; i++) {
+            int key = arr[i];
+            int j = i - 1;
 
+            while (j >= 0 && arr[j] > key) {
+                arr[j+1] = arr[j];
+                j = j - 1;
+            }
+            arr[j+1] = key;
+        }
+
+    }
 }
