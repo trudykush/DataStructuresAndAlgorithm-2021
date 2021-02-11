cc6dd7233f61a36dc291d1fac0502aea5bac048b
trudykush <kushalmishra91@gmail.com>
2021-02-11 23:11:00 +0000

    QuickSort - Java

--- src/Feb2021/sorting/java/QuickSort.java
+++ src/Feb2021/sorting/java/QuickSort.java
@@ -1,4 +1,21 @@
 package Feb2021.sorting.java;
 
+import javafx.scene.transform.Scale;
+
+import java.util.Scanner;
+
 public class QuickSort {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int numberOfCases = sc.nextInt();
+
+        while (numberOfCases-- > 0) {
+            int n = sc.nextInt();
+            int[] arr = new int[n];
+            for (int i = 0; i < n; i++) {
+                arr[i] = sc.nextInt();
+            }
+        }
+    }
 }
