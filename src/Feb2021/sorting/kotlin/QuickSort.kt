463b483d7f554e7d6050cc77916d04aaa3708487
trudykush <kushalmishra91@gmail.com>
2021-02-12 23:32:21 +0000

    QuickSort - Kotlin

--- src/Feb2021/sorting/kotlin/QuickSort.kt
+++ src/Feb2021/sorting/kotlin/QuickSort.kt
@@ -5,8 +5,8 @@ fun main(args: Array<String>) {
     val inputList: ArrayList<Int> =
             ArrayList(input!!.split(" ").map { it.toInt() })
 
-    /*quickSort(inputList.toIntArray(),
-                0, inputList.size-1)*/
+    quickSort(inputList.toIntArray(),
+                0, inputList.size-1)
     printSortedArray(inputList)
 }
 
@@ -15,5 +15,34 @@ fun printSortedArray(inputList: java.util.ArrayList<Int>) {
         print("${inputList[i]} ")
     }
     println()
+}
+
+fun quickSort(toArray: IntArray, low: Int, high: Int) {
+
+    if (low < high) {
+        val pivot = partition(toArray, low, high)
+
+        quickSort(toArray, low, pivot-1)
+        quickSort(toArray, pivot+1, high)
+    }
+}
+
+fun partition(toArray: IntArray, low: Int, high: Int): Int {
+    val pivot = toArray[high]
+    var i: Int = low - 1
+    for (j in low until high) {
+        if(toArray[j] < pivot) {
+            i++
+            val temp = toArray[i]
+            toArray[i] = toArray[j]
+            toArray[j] = temp
+        }
+    }
+
+    val temp = toArray[i+1]
+    toArray[i+1] = toArray[high]    // high or pivot
+    toArray[high] = temp
+
+    return i+1
 }
 
