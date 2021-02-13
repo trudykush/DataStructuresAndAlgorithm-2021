55d0057ff3022399747584f9baff2ac71091d44b
trudykush <kushalmishra91@gmail.com>
2021-02-13 00:00:36 +0000

    SortingForBinaryNumberArray - Java

--- src/Feb2021/sorting/java/SortingForBinaryNumberArray.java
+++ src/Feb2021/sorting/java/SortingForBinaryNumberArray.java
@@ -20,6 +20,30 @@ public class SortingForBinaryNumberArray {
     }
 
     private static void sorting(int[] arr, int n) {
+        int left = 0;
+        int right = n-1;
 
+        for (int i = 0; i < n / 2; i++) {
+            if (arr[left] == 1 && arr[right] == 0) {
+                int temp = arr[left];
+                arr[left] = arr[right];
+                arr[right] = temp;
+            }
+            if (arr[left] == 0) {
+                left++;
+            }
+            if (arr[right] == 1) {
+                right--;
+            }
+        }
+
+        printResult(arr);
+    }
+
+    private static void printResult(int[] arr) {
+        for (int j : arr) {
+            System.out.print(j + " ");
+        }
+        System.out.println();
     }
 }
