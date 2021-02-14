61fa869f9cd2f5d0302adf2c33918e9141db7871
trudykush <kushalmishra91@gmail.com>
2021-02-14 17:39:59 +0000

    SortingForBinaryNumberArray - Kotlin

--- src/Feb2021/sorting/kotlin/SortingForBinaryArray.kt
+++ src/Feb2021/sorting/kotlin/SortingForBinaryArray.kt
@@ -10,5 +10,24 @@ fun main(args: Array<String>) {
 }
 
 fun sortingBinaryArray(toIntArray: IntArray, size: Int) {
+    var left = 0
+    var right = size - 1
+
+    while (left < right) {
+        while (toIntArray[left] == 0 && left < right) {
+            left++
+        }
+        while (toIntArray[right] == 1 && left < right) {
+            right--
+        }
+        if (toIntArray[left] == 1 && toIntArray[right] == 0) {
+            val temp = toIntArray[left]
+            toIntArray[left] = toIntArray[right]
+            toIntArray[right] = temp
+
+            left++
+            right--
+        }
+    }
 
 }
