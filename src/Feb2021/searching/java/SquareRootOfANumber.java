2bf1d1890df1c047557c4a206bd080eb859cb8ce
trudykush <kushalmishra91@gmail.com>
2021-02-17 00:04:00 +0000

    Square root of a number - Java

---
+++ src/Feb2021/searching/java/SquareRootOfANumber.java
@@ -0,0 +1,18 @@
+package Feb2021.searching.java;
+
+import java.util.Scanner;
+
+// TODO: Find square root of a number or if not a perfect square, get floor number
+public class SquareRootOfANumber {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int numberOfCases = sc.nextInt();
+        while (numberOfCases-- > 0) {
+            int inputNumber = sc.nextInt();
+
+       //     squareRootOrFloorNumber(inputNumber);
+        }
+    }
+
+
+}
