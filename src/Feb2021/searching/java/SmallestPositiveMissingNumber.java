e9e003e01138c984db97ff721c1ffbf98db9663b
trudykush <kushalmishra91@gmail.com>
2021-02-18 00:02:35 +0000

    Searching smallest positive number

---
+++ src/Feb2021/searching/java/SmallestPositiveMissingNumber.java
@@ -0,0 +1,20 @@
+package Feb2021.searching.java;
+
+import java.util.Scanner;
+
+public class SmallestPositiveMissingNumber {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int numberOfCases = sc.nextInt();
+        while (numberOfCases-- > 0) {
+            int n = sc.nextInt();
+            int[] arr = new int[n];
+            for (int i = 0; i < n; i++) {
+                arr[i] = sc.nextInt();
+            }
+
+         //   int result = missingNumber(arr, n);
+        }
+    }
+
+}
