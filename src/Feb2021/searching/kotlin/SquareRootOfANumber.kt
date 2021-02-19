aa6f69e02fcd245fbf9a424fb8b9f1f85fa9f5ea
trudykush <kushalmishra91@gmail.com>
2021-02-19 18:48:17 +0000

    Square root of a number - Koltin

--- src/Feb2021/searching/kotlin/SquareRootOfANumber.kt
+++ src/Feb2021/searching/kotlin/SquareRootOfANumber.kt
@@ -2,9 +2,29 @@ package Feb2021.searching.kotlin
 
 fun main(args: Array<String>) {
     val input = readLine()
-    val inputList: ArrayList<Int> =
-            ArrayList(input!!.split(" ")
-                    .map { it.toInt() })
 
+    findingSquareRoot(input!!.toInt())
+}
 
+fun findingSquareRoot(input: Int?): Int {
+    var low = 0
+    var high = input
+    var ans = 0
+    while (low < high!!) {
+        if (input == 0 || input == 1) {
+            return 0
+        }
+        var mid = low + (high - low) / 2
+        var midSquare = mid * mid
+        if (midSquare == input) {
+            return midSquare
+        }
+        if (midSquare < input!!) {
+            low = mid + 1;
+            ans = mid
+        } else {
+            high = mid - 1
+        }
+    }
+    return ans
 }
