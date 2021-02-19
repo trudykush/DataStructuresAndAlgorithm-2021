ac9f10b66c48da7cce9100e41864329743b8811c
trudykush <kushalmishra91@gmail.com>
2021-02-19 18:05:17 +0000

    Square root of a number - Koltin

---
+++ src/Feb2021/searching/kotlin/SquareRootOfANumber.kt
@@ -0,0 +1,10 @@
+package Feb2021.searching.kotlin
+
+fun main(args: Array<String>) {
+    val input = readLine()
+    val inputList: ArrayList<Int> =
+            ArrayList(input!!.split(" ")
+                    .map { it.toInt() })
+
+
+}
