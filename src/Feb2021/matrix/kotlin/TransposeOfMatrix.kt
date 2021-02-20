4dd3f128cd66f5bd34c62f93c219ba8c3ed05bf4
trudykush <kushalmishra91@gmail.com>
2021-02-20 23:55:16 +0000

    Transpose Of Matrix - Kotlin

---
+++ src/Feb2021/matrix/kotlin/TransposeOfMatrix.kt
@@ -0,0 +1,9 @@
+package Feb2021.matrix.kotlin
+
+
+fun main(args: Array<String>) {
+    val input = readLine()
+    var inputNumber: ArrayList<Int> =
+            ArrayList(input!!.split(" ")
+                    .map { it.toInt() })
+}
