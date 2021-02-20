76b0f5c7bba76130231ce1888b1dc5411a9c848d
trudykush <kushalmishra91@gmail.com>
2021-02-20 23:58:08 +0000

    Transpose Of Matrix - Kotlin

--- src/Feb2021/matrix/kotlin/TransposeOfMatrix.kt
+++ src/Feb2021/matrix/kotlin/TransposeOfMatrix.kt
@@ -2,8 +2,22 @@ package Feb2021.matrix.kotlin
 
 
 fun main(args: Array<String>) {
+
+    // Number of rows
+    val rows = readLine()!!.toInt()
+
+    // Number of columns
+    val columns = readLine()!!.toInt()
+
     val input = readLine()
     var inputNumber: ArrayList<Int> =
             ArrayList(input!!.split(" ")
                     .map { it.toInt() })
+
+    gettingTransposeOfMatrix(inputNumber, rows, columns)
+
+}
+
+fun gettingTransposeOfMatrix(inputNumber: java.util.ArrayList<Int>, rows: Int, columns: Int) {
+
 }
