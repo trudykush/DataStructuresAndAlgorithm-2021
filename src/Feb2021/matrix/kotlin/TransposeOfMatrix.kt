7b62ec751aef7e7f9d31871fe7343c94dd0a7907
trudykush <kushalmishra91@gmail.com>
2021-02-21 00:50:26 +0000

    Transpose Of Matrix - Kotlin Messing with new ways

--- src/Feb2021/matrix/kotlin/TransposeOfMatrix.kt
+++ src/Feb2021/matrix/kotlin/TransposeOfMatrix.kt
@@ -4,24 +4,36 @@ package Feb2021.matrix.kotlin
 fun main(args: Array<String>) {
 
     // Number of rows
-    val rows = readLine()!!.toInt()
+    val rows = 2
 
     // Number of columns
-    val columns = readLine()!!.toInt()
+    val columns = 3
 
     val ogMatrix = arrayOf(intArrayOf(2,3,4), intArrayOf(5,6,7))
 
-    val inputMatrix = Array(rows) {IntArray(columns)}
+    displayTranspose(ogMatrix)
 
-    for (row in 0 until rows - 1) {
-        for (column in 0 until columns - 1) {
+    val inputMatrix = Array(columns) {IntArray(rows)}
+
+    for (row in 0 until rows) {
+        for (column in 0 until columns) {
             inputMatrix[column][row] = ogMatrix[row][column]
         }
     }
 
-
+     displayTranspose(inputMatrix)
     //gettingTransposeOfMatrix(inputNumber, rows, columns)
 
+}
+
+fun displayTranspose(inputMatrix: Array<IntArray>) {
+    println()
+    for (row in inputMatrix) {
+        for (column in row) {
+            print("$column  ")
+        }
+        println()
+    }
 }
 
 fun gettingTransposeOfMatrix(inputNumber: ArrayList<Int>,
