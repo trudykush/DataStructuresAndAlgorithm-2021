1638a985a5cb9887349cc983aeface0b4920c6e8
trudykush <kushalmishra91@gmail.com>
2021-02-22 22:14:12 +0000

    Binary Sub String - Java

--- src/Feb2021/strings/BinarySubString.java
+++ src/Feb2021/strings/BinarySubString.java
@@ -8,19 +8,21 @@ public class BinarySubString {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(
                             new InputStreamReader(System.in));
-        int numberOfCases = br.read();
+        System.out.println("Please enter the number of cases: ");
+        int numberOfCases = Integer.parseInt(br.readLine());
         while (numberOfCases-- > 0) {
+            System.out.println("Please enter the input string: ");
             String inputStr = br.readLine();
 
             int result = findingAllSubStringEndingWithOne(inputStr);
-            System.out.println(result);
+            System.out.println("Number of substring for given input is: " + result);
         }
     }
 
     private static int findingAllSubStringEndingWithOne(String str) {
         int count = 0;
         for (int i = 0; i < str.length(); i++) {
-            if(str.charAt(i) == 1) {
+            if(str.charAt(i) == '1') {
                 count++;
             }
         }
