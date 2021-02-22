ab2d35798290ce3181dd0faf766fe00236db1ee4
trudykush <kushalmishra91@gmail.com>
2021-02-22 22:05:13 +0000

    Binary Sub String - Java

--- src/Feb2021/strings/BinarySubString.java
+++ src/Feb2021/strings/BinarySubString.java
@@ -1,4 +1,22 @@
 package Feb2021.strings;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class BinarySubString {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(
+                            new InputStreamReader(System.in));
+        int numberOfCases = br.read();
+        while (numberOfCases-- > 0) {
+            int inputStr = br.read();
+
+            findingAllSubStringEndingWithOne(inputStr);
+        }
+    }
+
+    private static void findingAllSubStringEndingWithOne(int str) {
+
+    }
 }
