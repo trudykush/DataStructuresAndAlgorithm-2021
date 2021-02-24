56dea580caf653d8633c81e23a2e7c45ada73309
trudykush <kushalmishra91@gmail.com>
2021-02-24 00:15:21 +0000

    Anagram - Java

---
+++ src/Feb2021/strings/Anagram.java
@@ -0,0 +1,41 @@
+package Feb2021.strings;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Anagram {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter number of test cases: ");
+        int numberOfCases = sc.nextInt();
+        while (numberOfCases-- > 0) {
+            System.out.println("Enter first word: ");
+            String fw = sc.next();
+            System.out.println("Enter second word: ");
+            String sw = sc.next();
+
+            Boolean result = isAnagram(fw, sw);
+            System.out.println(result);
+        }
+    }
+
+    private static Boolean isAnagram(String fw, String sw) {
+        char[] fwChar = fw.toCharArray();
+        char[] swChar = sw.toCharArray();
+
+        if (fwChar.length != swChar.length) {
+            return false;
+        }
+
+        Arrays.sort(fwChar);
+        Arrays.sort(swChar);
+
+        for (int i = 0; i < fwChar.length; i++) {
+            if (fwChar[i] != swChar[i]) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
