da4f47780e3b135cbf0c70363efe6185ad1349fa
trudykush <kushalmishra91@gmail.com>
2021-02-28 22:26:07 +0000

    Anagram - Kotlin

---
+++ src/Feb2021/strings/kotlin/Anagram.kt
@@ -0,0 +1,18 @@
+package Feb2021.strings.kotlin
+
+fun main(args: Array<String>) {
+    val firstInputStr = readLine()
+    val secondInputStr = readLine()
+
+    val firstCharArray = firstInputStr!!.toCharArray()
+    val secondCharArray = secondInputStr!!.toCharArray()
+
+    firstCharArray.sort()
+    secondCharArray.sort()
+
+
+
+
+
+
+}
