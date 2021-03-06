a9e4e8863087e57a65eef86daa590c07d7bc25d0
trudykush <kushalmishra91@gmail.com>
2021-03-06 19:37:44 +0000

    IsomorphicStrings

--- src/March2021/strings/IsomorphicStrings.java
+++ src/March2021/strings/IsomorphicStrings.java
@@ -6,5 +6,16 @@ public class IsomorphicStrings {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int numberOfCases = sc.nextInt();
+
+        while (numberOfCases-- > 0) {
+            String firstStr  = sc.next();
+            String secondStr = sc.next();
+
+            isIsomorphic(firstStr, secondStr);
+        }
+    }
+
+    private static void isIsomorphic(String firstStr, String secondStr) {
+
     }
 }
