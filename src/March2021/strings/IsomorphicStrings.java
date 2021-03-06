d4efff91291a98195f0cd316e987dd2cfa533b58
trudykush <kushalmishra91@gmail.com>
2021-03-06 19:35:33 +0000

    IsomorphicStrings

---
+++ src/March2021/strings/IsomorphicStrings.java
@@ -0,0 +1,10 @@
+package March2021.strings;
+
+import java.util.Scanner;
+
+public class IsomorphicStrings {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int numberOfCases = sc.nextInt();
+    }
+}
