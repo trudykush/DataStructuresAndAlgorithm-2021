89d7e57b1d3fbc749fca2ddb1b4d1cf372582cf1
trudykush <kushalmishra91@gmail.com>
2021-03-07 23:06:29 +0000

    RotatedString  - Kotlin

--- src/March2021/strings/kotlin/RotatedString.kt
+++ src/March2021/strings/kotlin/RotatedString.kt
@@ -1,5 +1,16 @@
 package March2021.strings.kotlin
 
-fun main(args: Array<String>) {
 
+// TODO: Needs improvement, id doesn't check if string is same
+fun main(args: Array<String>) {
+    val firstStr = readLine()
+    val secondStr = readLine()
+
+    if ((firstStr!!.length == secondStr!!.length)
+            &&
+            (firstStr+secondStr).contains(secondStr)) {
+            print("Is rotated")
+        } else {
+            print("Not Rotated")
+    }
 }
