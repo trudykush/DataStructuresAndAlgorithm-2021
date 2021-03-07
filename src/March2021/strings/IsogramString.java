a286c5e0ccdc359b26c8443ec9937a6fd07ffa8c
trudykush <kushalmishra91@gmail.com>
2021-03-07 22:36:08 +0000

    Isogram String - print result

--- src/March2021/strings/IsogramString.java
+++ src/March2021/strings/IsogramString.java
@@ -1,5 +1,6 @@
 package March2021.strings;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class IsogramString {
@@ -9,11 +10,21 @@ public class IsogramString {
         while (numberOfCases-- > 0) {
             String inputStr = sc.next();
 
-            isIsogram(inputStr);
+            boolean result = isIsogram(inputStr);
+            System.out.println(result);
         }
     }
 
-    private static void isIsogram(String inputStr) {
-
+    private static boolean isIsogram(String inputStr) {
+        int[] map = new int[256];
+        Arrays.fill(map, -1);
+        for (int i = 0; i < inputStr.length(); i++) {
+            if (map[inputStr.charAt(i)] != -1) {
+                return false;
+            } else {
+                map[inputStr.charAt(i)] = inputStr.charAt(i);
+            }
+        }
+        return true;
     }
 }
