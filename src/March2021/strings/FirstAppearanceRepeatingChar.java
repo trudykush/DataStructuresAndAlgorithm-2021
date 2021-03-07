1aea044c48d291697d32bad15dc5ffe62aa3a2b9
trudykush <kushalmishra91@gmail.com>
2021-03-07 22:49:47 +0000

    First Appearance of repeated char in string

---
+++ src/March2021/strings/FirstAppearanceRepeatingChar.java
@@ -0,0 +1,24 @@
+package March2021.strings;
+
+import javax.swing.*;
+import java.util.Scanner;
+
+public class FirstAppearanceRepeatingChar {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int numberOfCases = sc.nextInt();
+        while (numberOfCases-- > 0) {
+            String inputStr = sc.next();
+            
+            getFirstAppearance(inputStr);
+        }
+    }
+
+    private static int getFirstAppearance(String inputStr) {
+        int result = -1;
+
+                
+        return result;
+    }
+
+}
