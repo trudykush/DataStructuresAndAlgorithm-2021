679e562a4fe2499c383550f0b61853a5bf823dd3
trudykush <kushalmishra91@gmail.com>
2021-03-13 00:50:53 +0000

    LinkedList counting - Iteration way

--- src/March2021/linkedList/CountNodesOfLinkedList.java
+++ src/March2021/linkedList/CountNodesOfLinkedList.java
@@ -10,6 +10,24 @@ public class CountNodesOfLinkedList {
             int n = sc.nextInt();
             Node head = new Node(sc.nextInt());
             Node tail = head;
+            for (int i = 0; i < n - 1; i++) {
+                tail.next = new Node(sc.nextInt());
+                tail = tail.next;
+            }
+            Solution ob = new Solution();
+            System.out.println(ob.getCount(head));
+        }
+    }
+
+    private static class Solution {
+        public int getCount(Node head) {
+            Node temp = head;
+            int count = 0;
+            while (temp != null) {
+                count++;
+                temp = temp.next;
+            }
+            return count;
         }
     }
 }
