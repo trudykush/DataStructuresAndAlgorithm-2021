69ae7428c395c77b0a03afdb664fc79b60dffd6d
trudykush <kushalmishra91@gmail.com>
2021-03-14 21:16:44 +0000

    LinkedList Insertion- Recursion way

---
+++ src/March2021/linkedList/LinkedListInsertion.java
@@ -0,0 +1,18 @@
+package March2021.linkedList;
+
+import java.util.Scanner;
+
+public class LinkedListInsertion {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int numberOfCases = sc.nextInt();
+        while (numberOfCases > 0) {
+            int n = sc.nextInt();
+            for (int i = 0; i < n; i++) {
+                int value = sc.nextInt();
+                int indicator = sc.nextInt();
+            }
+            numberOfCases--;
+        }
+    }
+}
