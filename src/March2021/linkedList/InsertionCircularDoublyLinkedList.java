401dcc0c2480b8c082d41a349e4347df417681a9
trudykush <kushalmishra91@gmail.com>
2021-03-16 00:34:30 +0000

    Doubly LinkedList - DLinkedList

--- src/March2021/linkedList/InsertionCircularDoublyLinkedList.java
+++ src/March2021/linkedList/InsertionCircularDoublyLinkedList.java
@@ -1,19 +1,42 @@
 package March2021.linkedList;
 
+import java.util.Scanner;
+
 public class InsertionCircularDoublyLinkedList {
 
     class Node {
         int data;
-        March2021.linkedList.Node next;
-        March2021.linkedList.Node prev;
+        Node next;
+        Node prev;
         Node(int data) {
             this.data = data;
             next = prev = null;
         }
     }
 
-    public static void main(String[] args) {
+    class DLinkedList {
+        Node newNode (Node head, int data) {
+            Node n = new Node(data);
+            if (head == null) {
+                head = n;
+                return head;
+            }
+            head.next = n;
+            n.prev = head;
+            head = n;
+            return head;
+        }
+    }
 
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int numberOfCases = sc.nextInt();
+        while (numberOfCases-- > 0) {
+            Node temp;
+            Node head = null;
+            Node root = null;
+            int n = sc.nextInt();
+        }
     }
 
 }
