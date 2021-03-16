d5c83d1b24837376071bd18d295c903c7af8f1b9
trudykush <kushalmishra91@gmail.com>
2021-03-16 00:18:31 +0000

    Doubly LinkedList - Insertion

---
+++ src/March2021/linkedList/InsertionCircularDoublyLinkedList.java
@@ -0,0 +1,19 @@
+package March2021.linkedList;
+
+public class InsertionCircularDoublyLinkedList {
+
+    class Node {
+        int data;
+        March2021.linkedList.Node next;
+        March2021.linkedList.Node prev;
+        Node(int data) {
+            this.data = data;
+            next = prev = null;
+        }
+    }
+
+    public static void main(String[] args) {
+
+    }
+
+}
