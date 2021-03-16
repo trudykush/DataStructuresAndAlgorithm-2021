eb814dfab4ce9d3d65c7260beb76f4f1badee128
trudykush <kushalmishra91@gmail.com>
2021-03-16 00:39:28 +0000

    Doubly LinkedList - DLinkedList

--- src/March2021/linkedList/InsertionCircularDoublyLinkedList.java
+++ src/March2021/linkedList/InsertionCircularDoublyLinkedList.java
@@ -14,21 +14,21 @@ public class InsertionCircularDoublyLinkedList {
         }
     }
 
-    class DLinkedList {
-        Node newNode (Node head, int data) {
-            Node n = new Node(data);
-            if (head == null) {
-                head = n;
-                return head;
-            }
-            head.next = n;
-            n.prev = head;
+    Node newNode (Node head, int data) {
+        Node n = new Node(data);
+        if (head == null) {
             head = n;
             return head;
         }
+        head.next = n;
+        n.prev = head;
+        head = n;
+        return head;
     }
 
     public static void main(String[] args) {
+        InsertionCircularDoublyLinkedList dLL =
+                new InsertionCircularDoublyLinkedList();
         Scanner sc = new Scanner(System.in);
         int numberOfCases = sc.nextInt();
         while (numberOfCases-- > 0) {
@@ -36,7 +36,24 @@ public class InsertionCircularDoublyLinkedList {
             Node head = null;
             Node root = null;
             int n = sc.nextInt();
+            for (int i = 0; i < n; i++) {
+                int x = sc.nextInt();
+                head = dLL.newNode(head, x);
+                if (root == null) {
+                    root = head;
+                }
+            }
+            head = root;
+            int pos = sc.nextInt();
+            int data = sc.nextInt();
+            SolutionDLL solution = new SolutionDLL();
+            solution.addNode(head, pos, data);
         }
     }
 
+    private static class SolutionDLL {
+        public void addNode(Node head, int pos, int data) {
+
+        }
+    }
 }
