ffe2f9cdc05cc933399b2f780deaf25f9d26ed2f
trudykush <kushalmishra91@gmail.com>
2021-03-20 01:05:12 +0000

    MaximumOccuringChar - Lexographically

--- src/March2021/strings/java/MaximumOccuringChar.java
+++ src/March2021/strings/java/MaximumOccuringChar.java
@@ -3,6 +3,7 @@ package March2021.strings.java;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 
 public class MaximumOccuringChar {
     public static void main(String[] args) throws IOException {
@@ -16,7 +17,13 @@ public class MaximumOccuringChar {
     }
 
     private static char maxOccuranceChar(String inputStr) {
-
+        int[] map = new int[256];
+        Arrays.fill(map, -1);
+        for (int i = 0; i < inputStr.length(); i++) {
+            if (map[inputStr.charAt(i)] == -1) {
+                map[inputStr.charAt(i)] = map[inputStr.charAt(i)] + 1;
+            }
+        }
         return 0;
     }
 }
