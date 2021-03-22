0497239aba2accc0840bd0df54b4406cdb7da0ab
trudykush <kushalmishra91@gmail.com>
2021-03-22 22:00:46 +0000

    Remove uncommon char and concatenate

--- src/March2021/strings/java/RemoveUncommonCharAndConcatenate.java
+++ src/March2021/strings/java/RemoveUncommonCharAndConcatenate.java
@@ -1,4 +1,13 @@
 package March2021.strings.java;
 
+import java.util.Scanner;
+
 public class RemoveUncommonCharAndConcatenate {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int numberOfCases = sc.nextInt();
+        while (numberOfCases-- > 0) {
+
+        }
+    }
 }
