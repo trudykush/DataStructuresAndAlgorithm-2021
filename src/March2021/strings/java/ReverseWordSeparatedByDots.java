583a167a9cfa0d55d3f7adfc1ac4d7cd49431634
trudykush <kushalmishra91@gmail.com>
2021-03-23 22:30:42 +0000

    Reverse Words Separated By Dots

--- src/March2021/strings/java/ReverseWordSeparatedByDots.java
+++ src/March2021/strings/java/ReverseWordSeparatedByDots.java
@@ -1,9 +1,18 @@
 package March2021.strings.java;
 
-import java.util.Scanner;
+import java.util.*;
 
 public class ReverseWordSeparatedByDots {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-    }
+        int numberOfCases = sc.nextInt();
+        while (numberOfCases-- > 0) {
+            String str = sc.next();
+            String[] separatedString = new String[str.length()];
+            separatedString = str.split("\\.");
+            Collections.reverse(Arrays.asList(separatedString));
+            System.out.println(Arrays.toString(separatedString));
+
+        }
+     }
 }
