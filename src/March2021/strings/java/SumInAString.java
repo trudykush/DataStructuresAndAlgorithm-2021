8d6f27fc374b73ea92993028f7423718c30c36f4
trudykush <kushalmishra91@gmail.com>
2021-03-24 01:19:14 +0000

    Sum in a string

---
+++ src/March2021/strings/java/SumInAString.java
@@ -0,0 +1,20 @@
+package March2021.strings.java;
+
+import java.util.Scanner;
+
+public class SumInAString {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String str = sc.next();
+
+        findSum(str);
+    }
+
+    private static int findSum(String str) {
+        // A temporary String
+        String temp = "0";
+
+        // holds
+        return -1;
+    }
+}
