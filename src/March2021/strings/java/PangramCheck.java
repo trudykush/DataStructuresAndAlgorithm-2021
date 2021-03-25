5bd89ad26ff1b529292e00a5ae6e25d229814277
trudykush <kushalmishra91@gmail.com>
2021-03-25 00:56:52 +0000

    Pangram Check

--- src/March2021/strings/java/PangramCheck.java
+++ src/March2021/strings/java/PangramCheck.java
@@ -5,5 +5,27 @@ import java.util.Scanner;
 public class PangramCheck {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        int numberOfCases = sc.nextInt();
+        while (numberOfCases-- > 0) {
+            String str = sc.next();
+
+            checkingIfPangram(str);
+        }
+    }
+
+    private static boolean checkingIfPangram(String str) {
+        boolean[] mark = new boolean[26];
+        int index = 0;
+        for (int i = 0; i < str.length(); i++) {
+            if ('A' <= str.charAt(i) && str.charAt(i) <= 'Z') {
+                index = str.charAt(i) - 'A';
+            } else if ('a' <= str.charAt(i) && str.charAt(i) <= 'z') {
+                index = str.charAt(i) - 'a';
+            } else {
+                continue;
+            }
+            mark[index] = true;
+        }
+
     }
 }
