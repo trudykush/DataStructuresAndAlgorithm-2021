023bcd1da1d572d4a143f99727b51cf6543e35f1
trudykush <kushalmishra91@gmail.com>
2021-03-25 01:05:32 +0000

    Pangram Check - commenting

--- src/March2021/strings/java/PangramCheck.java
+++ src/March2021/strings/java/PangramCheck.java
@@ -5,8 +5,10 @@ import java.util.Scanner;
 public class PangramCheck {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        System.out.println("Enter total number of test-cases to be performed:");
         int numberOfCases = sc.nextInt();
         while (numberOfCases-- > 0) {
+            System.out.println("Please enter the input String:");
             String str = sc.next();
 
             boolean result = checkingIfPangram(str);
