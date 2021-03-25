7065505eb7e8e94ecb1ecb700f61873d86dcd517
trudykush <kushalmishra91@gmail.com>
2021-03-25 00:48:39 +0000

    Pangram Check

---
+++ src/March2021/strings/java/PangramCheck.java
@@ -0,0 +1,9 @@
+package March2021.strings.java;
+
+import java.util.Scanner;
+
+public class PangramCheck {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+    }
+}
