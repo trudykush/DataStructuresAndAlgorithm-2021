06760f46e28b5c2b7a3fc8ed6e74530a785ace48
trudykush <kushalmishra91@gmail.com>
2021-03-25 01:03:22 +0000

    Pangram Check - commenting

--- src/March2021/strings/java/PangramCheck.java
+++ src/March2021/strings/java/PangramCheck.java
@@ -15,24 +15,36 @@ public class PangramCheck {
     }
 
     private static boolean checkingIfPangram(String str) {
+        // create a hash table to mark the characters present in the string
+        // by default all the elements of mark would be false
         boolean[] mark = new boolean[26];
+        // For indexing in mark[]
         int index = 0;
+        // Traverse all characters
         for (int i = 0; i < str.length(); i++) {
+            // If uppercase char, subtract 'A' to find index
             if ('A' <= str.charAt(i) && str.charAt(i) <= 'Z') {
                 index = str.charAt(i) - 'A';
-            } else if ('a' <= str.charAt(i) && str.charAt(i) <= 'z') {
+            }
+            // If lowercase char, subtract 'a' to find index
+            else if ('a' <= str.charAt(i) && str.charAt(i) <= 'z') {
                 index = str.charAt(i) - 'a';
-            } else {
+            }
+            // If this char is other than english
+            // lowercase and uppercase char
+            else {
                 continue;
             }
             mark[index] = true;
         }
 
+        // return false if any char is unmarked
         for (int i = 0; i < 25; i++) {
             if (!mark[i]) {
                 return false;
             }
         }
+        // if all char are present
         return true;
     }
 }
