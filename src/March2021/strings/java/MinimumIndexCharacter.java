6db49292faf76c26dac716e4185373f4f26422ad
trudykush <kushalmishra91@gmail.com>
2021-03-26 22:14:09 +0000

    Minimum Index Char

--- src/March2021/strings/java/MinimumIndexCharacter.java
+++ src/March2021/strings/java/MinimumIndexCharacter.java
@@ -15,31 +15,40 @@ public class MinimumIndexCharacter {
             System.out.println("Please enter the pattern string");
             String patternStr = sc.next();
 
-            char result = findingMinIndexChar(inputStr, patternStr);
-            if (result != 0) {
-                System.out.println(result);
-            }
+            printMinIndexChar(inputStr, patternStr);
         }
     }
 
     static void printMinIndexChar(String str, String pat) {
+        // map to store the first index of each char of 'str'
         HashMap<Character, Integer> hm = new HashMap<>();
+
+        // to store the index of char having min index
         int minIndex = Integer.MAX_VALUE;
+
+        // length of both strings
         int m = str.length();
         int n = pat.length();
+
+        // store the first index of each char of 'str'
         for (int i = 0; i < m; i++) {
             if (!hm.containsKey(str.charAt(i))) {
                 hm.put(str.charAt(i), i);
             }
         }
 
+        // traverse the string 'pat'
         for (int i = 0; i < n; i++) {
+            // if pat[i] is found in 'str', check if
+            // it has the min index or not accordingly
+            // update 'minIndex'
             if(hm.containsKey(pat.charAt(i)) &&
                     hm.get(pat.charAt(i)) < minIndex) {
                 minIndex = hm.get(pat.charAt(i));
             }
         }
 
+        // print the min index char if present
         if (minIndex != Integer.MAX_VALUE) {
             System.out.println("Minimum Index Char = " +
                         str.charAt(minIndex));
