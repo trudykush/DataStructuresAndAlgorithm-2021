c36cb8786bd3794419e0db0f9dce515beada5907
trudykush <kushalmishra91@gmail.com>
2021-03-26 20:26:03 +0000

    Minimum Index Char

--- src/March2021/strings/java/MinimumIndexCharacter.java
+++ src/March2021/strings/java/MinimumIndexCharacter.java
@@ -5,9 +5,20 @@ import java.util.Scanner;
 public class MinimumIndexCharacter {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        System.out.println("Please enter number of cases: ");
         int numberOfCases = sc.nextInt();
         while (numberOfCases-- > 0) {
+            System.out.println("Please enter the input string");
+            String inputStr = sc.next();
+            System.out.println("Please enter the pattern string");
+            String patternStr = sc.next();
 
+            char result = findingMinIndexChar(inputStr, patternStr);
         }
+    }
+
+    private static char findingMinIndexChar(String inputStr, String patternStr) {
+
+        return 0;
     }
 }
