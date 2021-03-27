6a5165f3bf6b24d11899a318eb072a809019f81a
trudykush <kushalmishra91@gmail.com>
2021-03-27 15:36:38 +0000

    Non repeating string commenting

--- src/March2021/strings/kotlin/NonRepeatingString.kt
+++ src/March2021/strings/kotlin/NonRepeatingString.kt
@@ -3,10 +3,14 @@ package March2021.strings.kotlin
 import java.util.*
 
 fun main(args: Array<String>) {
+    // Reading input from user
     val inputStr = readLine()
+    // changing string into char array
     val inputChar = inputStr!!.toCharArray()
 
+    // making int array of size 256 and populating with -1
     var map = IntArray(256) {-1}
+    // for loop with index
     for ((index, value) in inputChar.withIndex()) {
         map[inputChar[index].toInt()] = map[inputChar[index].toInt()] + 1
     }
