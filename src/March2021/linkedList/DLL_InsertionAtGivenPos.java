5c77348d28af6e7634411989f803d2af3f549760
trudykush <kushalmishra91@gmail.com>
2021-03-30 23:16:40 +0100

    Doubly LinkedList at a given position

---
+++ src/March2021/linkedList/DLL_InsertionAtGivenPos.java
@@ -0,0 +1,17 @@
+package March2021.linkedList;
+
+public class DLL_InsertionAtGivenPos {
+
+    class Node {
+        int data;
+        Node next;
+        Node prev;
+        Node(int data) {
+            this.data = data;
+            next = prev = null;
+        }
+    }
+
+
+
+}
