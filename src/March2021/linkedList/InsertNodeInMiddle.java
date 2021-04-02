247a1b91adf561107ee7fd77b59ef1d95c99d915
trudykush <kushalmishra91@gmail.com>
2021-04-02 20:49:16 +0100

    LinkedList - InsertNodeInMiddle

---
+++ src/March2021/linkedList/InsertNodeInMiddle.java
@@ -0,0 +1,15 @@
+package March2021.linkedList;
+
+public class InsertNodeInMiddle {
+    static Node head;
+
+    static class Node {
+        int data;
+        Node next;
+
+        Node(int d) {
+            this.data = d;
+            next = null;
+        }
+    }
+}
