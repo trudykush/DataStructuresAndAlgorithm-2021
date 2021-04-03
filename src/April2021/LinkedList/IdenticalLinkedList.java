4ab1c32ce9b2217cfcf200f73a72de5e9057f2b4
trudykush <kushalmishra91@gmail.com>
2021-04-03 22:15:32 +0100

    Identical LinkedList

--- src/April2021/LinkedList/IdenticalLinkedList.java
+++ src/April2021/LinkedList/IdenticalLinkedList.java
@@ -53,7 +53,14 @@ public class IdenticalLinkedList {
 
     private static class Solution {
         public boolean isIdentical(Node head1, Node head2) {
-            return false;
+            while (head1 != null && head2 != null) {
+                if (head1.data != head2.data) {
+                    return false;
+                }
+                head1 = head1.next;
+                head2 = head2.next;
+            }
+            return (head1 == null && head2 == null);
         }
     }
 }
