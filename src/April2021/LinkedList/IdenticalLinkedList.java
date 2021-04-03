c8af5e17574ebb606442a64db0f8de2bdb759294
trudykush <kushalmishra91@gmail.com>
2021-04-03 22:01:25 +0100

    Identical LinkedList

---
+++ src/April2021/LinkedList/IdenticalLinkedList.java
@@ -0,0 +1,14 @@
+package April2021.LinkedList;
+
+public class IdenticalLinkedList {
+
+}
+
+class Node {
+    int data;
+    Node next;
+    public Node(int d) {
+        this.data = d;
+        this.next = null;
+    }
+}
