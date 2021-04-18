3d74cb8e03e8e922f886eaa68de002bc508a179e
trudykush <kushalmishra91@gmail.com>
2021-04-18 18:37:31 +0100

    Delete Without header pointer

--- src/April2021/LinkedList/DeleteWithoutHeaderPointer.java
+++ src/April2021/LinkedList/DeleteWithoutHeaderPointer.java
@@ -13,18 +13,37 @@ public class DeleteWithoutHeaderPointer {
         }
     }
 
-    void printList(Node node) {
+    static void printList(Node node) {
         while (node != null) {
             System.out.print(node.data + " ");
             node = node.next;
         }
     }
 
-    void deleteHeadNode(Node head) {
+    static void deleteHeadNode(Node head) {
         Node temp = head.next;
         head.data = temp.data;
         head.next = temp.next;
         System.gc();
     }
 
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int numberOfCases = sc.nextInt();
+        while (numberOfCases-- > 0) {
+            int n = sc.nextInt();
+            Node head = new Node(sc.nextInt());
+            Node tail = head;
+            for (int i = 0; i < n; i++) {
+                tail.next = new Node(sc.nextInt());
+                tail = tail.next;
+            }
+        }
+
+        printList(head);
+
+        deleteHeadNode(head);
+
+        printList(head);
+    }
 }
