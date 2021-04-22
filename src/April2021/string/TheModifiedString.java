07d49fd058d2735cc4f253223a69c1d3c8a56b20
trudykush <kushalmishra91@gmail.com>
2021-04-22 22:56:04 +0100

    The Modified String

--- src/April2021/string/TheModifiedString.java
+++ src/April2021/string/TheModifiedString.java
@@ -1,4 +1,22 @@
 package April2021.string;
 
+import java.util.Scanner;
+
 public class TheModifiedString {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        long t = sc.nextLong();
+
+        while (t-- > 0) {
+            String inputString = sc.next();
+            System.out.println(new Solution().modified(inputString));
+        }
+    }
+
+    private static class Solution {
+        public long modified(String inputString) {
+
+            return 0;
+        }
+    }
 }
