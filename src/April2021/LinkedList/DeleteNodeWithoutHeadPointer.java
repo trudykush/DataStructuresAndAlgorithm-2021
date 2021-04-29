34b7af1b3af4d4c187110918ae1384e1f31d05f2
trudykush <kushalmishra91@gmail.com>
2021-04-29 21:56:27 +0100

    Delete node without head pointer

--- src/April2021/LinkedList/DeleteNodeWithoutHeadPointer.java
+++ src/April2021/LinkedList/DeleteNodeWithoutHeadPointer.java
@@ -1,4 +1,22 @@
 package April2021.LinkedList;
 
 public class DeleteNodeWithoutHeadPointer {
+    static Node head;
+
+    static class Node {
+        int data;
+        Node next;
+        Node(int d) {
+            this.data = d;
+            next = null;
+        }
+    }
+
+    static void display() {
+        Node temp = head;
+        while (temp != null) {
+            System.out.println(temp.data + " ");
+            temp = temp.next;
+        }
+    }
 }
