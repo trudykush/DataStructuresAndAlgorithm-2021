de8a980e2fb71afedecfa44d0a76ff607f2ac458
trudykush <kushalmishra91@gmail.com>
2021-05-11 20:06:40 +0100

    Array index and value match

---
+++ src/May2021/test/ArrayIndexAndValueMatch.java
@@ -0,0 +1,40 @@
+package May2021.test;
+
+public class ArrayIndexAndValueMatch {
+
+    public static void main(String [] args) {
+        // you can write to stdout for debugging purposes, e.g.
+        System.out.println("This is a debug message");
+        int[] arr = {-3, -1, 1, 3, 5};
+        int[] arr1 = {0, 0, 1};
+
+
+        ArrayIndexAndValueMatch obj = new ArrayIndexAndValueMatch();
+        int index = obj.binarySearch(arr1);
+        System.out.print(index);
+
+    }
+
+
+    // input array => arr
+    // number to find -> x
+    int binarySearch(int arr[]) {
+        int l = 0, r = arr.length - 1;
+        while(l <= r) {
+            int mid = l + (r - l)/2;
+
+            if (arr[mid] == mid) {
+                return mid;
+            }
+
+            if (arr[mid] < mid) {
+                l = mid + 1;
+            }
+            else {
+                r = mid - 1;
+            }
+        }
+        return -1;
+    }
+}
+
