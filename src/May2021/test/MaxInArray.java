9b80997e924d0d7fe17d96e6fa0626883ee9d8f8
trudykush <kushalmishra91@gmail.com>
2021-05-12 12:05:54 +0100

    Max in array

---
+++ src/May2021/test/MaxInArray.java
@@ -0,0 +1,27 @@
+package May2021.test;
+
+import java.util.Arrays;
+
+public class MaxInArray {
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 45, 8};
+
+        int max = Arrays.stream(arr).max().getAsInt();
+
+    }
+
+    public int maxNumber(int[] arr) {
+        int max = Integer.MIN_VALUE;
+        if (arr.length > 0) {
+            max = arr[0];
+
+            for (int i = 0; i < arr.length; i++) {
+                if (arr[i] > max) {
+                    max = arr[i];
+                }
+            }
+        }
+        return max;
+    }
+
+}
