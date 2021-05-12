de1fa2e7d8465a134f797be0803c54ea0afe6abe
trudykush <kushalmishra91@gmail.com>
2021-05-12 21:02:46 +0100

    Splitting word

---
+++ src/May2021/test/InterviewPart4.java
@@ -0,0 +1,18 @@
+package May2021.test;
+
+public class InterviewPart4 {
+
+    public static void main(String[] args) {
+
+        String inputStr = "Hi I can   fly";
+        String[] stringArr = inputStr.split(" ");
+        int counter = 0;
+
+        for (String s: stringArr) {
+            counter++;
+            System.out.println(s);
+        }
+
+        System.out.println(counter);
+    }
+}
