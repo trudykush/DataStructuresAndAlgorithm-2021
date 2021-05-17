a3ce080c051ecdb77564c850141a624d383f0b87
trudykush <kushalmishra91@gmail.com>
2021-05-17 21:47:09 +0100

    Removing Consecutive duplicates

--- src/May2021/String/RemovingConsecutiveDuplicates.java
+++ src/May2021/String/RemovingConsecutiveDuplicates.java
@@ -1,7 +1,34 @@
 package May2021.String;
 
-public class RemovingConsecutiveDuplicates {
-    public static void main(String[] args) {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.lang.reflect.Array;
+import java.util.Arrays;
 
+public class RemovingConsecutiveDuplicates {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(br.readLine().trim());
+        while (t-- > 0) {
+            String str = br.readLine().trim();
+            System.out.println(removeConseDuplicates(str.toCharArray()));
+        }
+    }
+
+    private static String removeConseDuplicates(char[] str) {
+
+        int n = str.length;
+        if (n < 2) {
+            return String.valueOf(str[0]);
+        }
+        int j = 0;
+        for (int i = 0; i < n; i++) {
+            if (str[j] != str[i]) {
+                j++;
+                str[j] = str[i];
+            }
+        }
+        return String.valueOf(Arrays.copyOfRange(str, 0, j+1));
     }
 }
