c5dbb87d63dd3f0f5ae7e4a759fe6e3795bea34b
trudykush <kushalmishra91@gmail.com>
2021-05-18 22:10:09 +0100

    Balanced Brackers

--- src/May2021/Arrays/BalancedBrackets.java
+++ src/May2021/Arrays/BalancedBrackets.java
@@ -1,4 +1,20 @@
 package May2021.Arrays;
 
+import java.util.Scanner;
+
 public class BalancedBrackets {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String inputStr = sc.next();
+
+        if (areBracketsBalanced(inputStr)) {
+            System.out.println("Balanced");
+        } else {
+            System.out.println("Not Balanced");
+        }
+    }
+
+    private static boolean areBracketsBalanced(String inputStr) {
+        return false;
+    }
 }
