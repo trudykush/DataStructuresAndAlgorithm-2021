a61506c0c512678169db894f701dccc3ec459d60
trudykush <kushalmishra91@gmail.com>
2021-05-18 23:50:04 +0100

    Balanced Brackets

--- src/May2021/Arrays/BalancedBrackets.java
+++ src/May2021/Arrays/BalancedBrackets.java
@@ -40,10 +40,19 @@ public class BalancedBrackets {
                     }
                     break;
                 case '}':
-
+                    check = stack.pop();
+                    if (check == '(' || check == '[') {
+                        return false;
+                    }
+                    break;
+                case ']':
+                    check = stack.pop();
+                    if (check == '(' || check == '}') {
+                        return false;
+                    }
                     break;
             }
         }
-        return false;
+        return (stack.isEmpty());
     }
 }
