74104d7c0c17826245926ea87c653801745147db
trudykush <kushalmishra91@gmail.com>
2021-05-18 23:47:21 +0100

    Balanced Brackets

--- src/May2021/Arrays/BalancedBrackets.java
+++ src/May2021/Arrays/BalancedBrackets.java
@@ -1,5 +1,7 @@
 package May2021.Arrays;
 
+import java.util.ArrayDeque;
+import java.util.Deque;
 import java.util.Scanner;
 
 public class BalancedBrackets {
@@ -15,6 +17,33 @@ public class BalancedBrackets {
     }
 
     private static boolean areBracketsBalanced(String inputStr) {
+
+        Deque<Character> stack = new ArrayDeque<>();
+
+        for (int i = 0; i < inputStr.length(); i++) {
+            char x = inputStr.charAt(i);
+
+            if(x == '(' || x == '[' || x == '{') {
+                stack.push(x);
+                continue;
+            }
+
+            if (stack.isEmpty()) {
+                return false;
+            }
+            char check;
+            switch (x) {
+                case ')':
+                    check = stack.pop();
+                    if (check == '{' || check == '[') {
+                        return false;
+                    }
+                    break;
+                case '}':
+
+                    break;
+            }
+        }
         return false;
     }
 }
