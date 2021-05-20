554ea3850225c929477d4b948d757ba8992e9ad5
trudykush <kushalmishra91@gmail.com>
2021-05-20 20:11:10 +0100

    SAP

--- src/May2021/SAP/CodingRound.java
+++ src/May2021/SAP/CodingRound.java
@@ -1,4 +1,39 @@
 package May2021.SAP;
 
+
+/*
+* Reverse array,  palindrome
+* */
 public class CodingRound {
+    public static void main(String[] args) {
+        String input = "hello";
+
+        char[] inputArray = input.toCharArray();
+        for (int i = inputArray.length - 1; i >= 0; i--) {
+            System.out.println(inputArray[i]);
+        }
+    }
+
+
+    class Node {
+        int key ;
+        Node left, right;
+
+        public Node(int item) {
+            key = item;
+            left = right = null;
+        }
+    }
+
+    void printInorder(Node node) {
+        if (node == null) {
+            return;
+        }
+        printInorder(node.left);
+        System.out.println(node.key + " ");
+        printInorder(node.right);
+    }
 }
+
+
+
