c9c915561244f74ba65342cb924dd725d993a367
trudykush <kushalmishra91@gmail.com>
2021-05-31 21:52:05 +0100

    Reverse a linked list

--- src/May2021/linkedList/ReverseALinkedList.java
+++ src/May2021/linkedList/ReverseALinkedList.java
@@ -1,4 +1,16 @@
 package May2021.linkedList;
 
+import java.util.Scanner;
+
 public class ReverseALinkedList {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int numberOfCases = sc.nextInt();
+        while (numberOfCases-- > 0) {
+            int n = sc.nextInt();
+            for (int i = 0; i < n; i++) {
+
+            }
+        }
+    }
 }
