e70978a4a64a42eb8eee5a3cc527c04ef85a3f45
trudykush <kushalmishra91@gmail.com>
2021-05-31 21:54:05 +0100

    Reverse a linked list

--- src/May2021/linkedList/ReverseALinkedList.java
+++ src/May2021/linkedList/ReverseALinkedList.java
@@ -8,9 +8,21 @@ public class ReverseALinkedList {
         int numberOfCases = sc.nextInt();
         while (numberOfCases-- > 0) {
             int n = sc.nextInt();
+            Node head = null;
+            Solution obj = new Solution();
             for (int i = 0; i < n; i++) {
-
+                int value = sc.nextInt();
+                head = obj.insertAtBeginning(head, value);
             }
         }
+    }
+
+    private static class Solution {
+        public Node insertAtBeginning(Node head, int value) {
+            return null;
+        }
+    }
+
+    private static class Node {
     }
 }
