38cc5df5ec904dad7efc2747e3d3d1add53d1527
trudykush <kushalmishra91@gmail.com>
2021-05-31 22:06:14 +0100

    Reverse a linked list

--- src/May2021/linkedList/ReverseALinkedList.java
+++ src/May2021/linkedList/ReverseALinkedList.java
@@ -54,8 +54,17 @@ public class ReverseALinkedList {
         }
 
         public Node reverse(Node head) {
-
-            return null;
+            Node prev = null;
+            Node current = head;
+            Node next = null;
+            while (current != null) {
+                next = current.next;
+                current.next = prev;
+                prev = current;
+                current = next;
+            }
+            head = prev;
+            return head;
         }
     }
 
