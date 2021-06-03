d78fbcf115387ff9191a51215ed39ec6874eb371
trudykush <kushalmishra91@gmail.com>
2021-06-03 22:54:32 +0100

    Remove Duplicate element from sorted linked list

--- src/June2021/LinkedList/RemoveDuplicateElementFromSortedLL.java
+++ src/June2021/LinkedList/RemoveDuplicateElementFromSortedLL.java
@@ -1,4 +1,13 @@
 package June2021.LinkedList;
 
+import java.util.Scanner;
+
 public class RemoveDuplicateElementFromSortedLL {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int numberOfCases = sc.nextInt();
+        while (numberOfCases-- > 0) {
+
+        }
+    }
 }
