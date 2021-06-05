cbd9eeda6c8f03436341b790a5926acfdc9b460f
trudykush <kushalmishra91@gmail.com>
2021-06-05 21:28:43 +0100

    Implement two stack in an array

--- src/June2021/Stack/ImplementTwoStackInArray.java
+++ src/June2021/Stack/ImplementTwoStackInArray.java
@@ -3,24 +3,25 @@ package June2021.Stack;
 import java.util.Scanner;
 
 public class ImplementTwoStackInArray {
+
+    int size;
+    int top1, top2;
+    int arr[] = new int[100];
+
+    ImplementTwoStackInArray() {
+        size = 100;
+        top1 = -1;
+        top2 = size;
+    }
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int numberOfCases = sc.nextInt();
         while (numberOfCases-- > 0) {
+            ImplementTwoStackInArray sq = new ImplementTwoStackInArray();
+            int q = sc.nextInt();
 
         }
     }
 
-
-    class TwoStack {
-        int size;
-        int top1, top2;
-        int arr[] = new int[100];
-
-        TwoStack() {
-            size = 100;
-            top1 = -1;
-            top2 = size;
-        }
-    }
 }
