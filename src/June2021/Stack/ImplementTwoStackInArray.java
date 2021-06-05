0812f508b4cefe3d3c3d7abd3155d644fe099ce5
trudykush <kushalmishra91@gmail.com>
2021-06-05 21:27:08 +0100

    Implement two stack in an array

--- src/June2021/Stack/ImplementTwoStackInArray.java
+++ src/June2021/Stack/ImplementTwoStackInArray.java
@@ -16,5 +16,11 @@ public class ImplementTwoStackInArray {
         int size;
         int top1, top2;
         int arr[] = new int[100];
+
+        TwoStack() {
+            size = 100;
+            top1 = -1;
+            top2 = size;
+        }
     }
 }
