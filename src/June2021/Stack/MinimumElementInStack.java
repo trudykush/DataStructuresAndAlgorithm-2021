108ca5add6b0914362c9c4ff10607b699cf4bab8
trudykush <kushalmishra91@gmail.com>
2021-06-07 20:04:53 +0100

    Minimum element in a stack

--- src/June2021/Stack/MinimumElementInStack.java
+++ src/June2021/Stack/MinimumElementInStack.java
@@ -2,6 +2,34 @@ package June2021.Stack;
 
 public class MinimumElementInStack {
     public static void main(String[] args) {
+        MyStack s = new MyStack();
+        s.push(3);
+        s.push(5);
+        s.getMin();
+        s.push(2);
+        s.push(1);
+        s.getMin();
+        s.pop();
+        s.getMin();
+        s.pop();
+        s.peek();
+    }
 
+    private static class MyStack {
+        public void push(int x) {
+
+        }
+
+        public void getMin() {
+
+        }
+
+        public void pop() {
+
+        }
+
+        public void peek() {
+
+        }
     }
 }
