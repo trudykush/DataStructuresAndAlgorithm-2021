a7921db443f66962a5cb8e504a9fdf50bf06b893
trudykush <kushalmishra91@gmail.com>
2021-06-08 22:14:51 +0100

    Delete middle element in stack

--- src/June2021/Stack/DeleteMiddleElementStack.java
+++ src/June2021/Stack/DeleteMiddleElementStack.java
@@ -1,4 +1,14 @@
 package June2021.Stack;
 
+import java.util.Scanner;
+
 public class DeleteMiddleElementStack {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int numberOfCases = sc.nextInt();
+
+        while (numberOfCases-- > 0) {
+
+        }
+    }
 }
