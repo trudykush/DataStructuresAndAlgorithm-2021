d2d0e11b07221e9b18bce6c83836c49721548616
trudykush <kushalmishra91@gmail.com>
2021-06-08 23:01:24 +0100

    Delete middle element in stack

--- src/June2021/Stack/DeleteMiddleElementStack.java
+++ src/June2021/Stack/DeleteMiddleElementStack.java
@@ -7,8 +7,29 @@ public class DeleteMiddleElementStack {
         Scanner sc = new Scanner(System.in);
         int numberOfCases = sc.nextInt();
 
+        MyStackLinkedList myStack = new MyStackLinkedList();
         while (numberOfCases-- > 0) {
+            int n = sc.nextInt();
+            for (int i = 0; i < n; i++) {
+                int queryType = sc.nextInt();
+                if (queryType == 1) {   // push
+                    int x = sc.nextInt();
+                    myStack.push(x);
+                } else { // pop
+                    int x = sc.nextInt();
+                    System.out.println(myStack.pop() + " ");
+                }
+            }
+        }
+    }
 
+    private static class MyStackLinkedList {
+        public void push(int x) {
+
+        }
+
+        public int pop() {
+            return -1;
         }
     }
 }
