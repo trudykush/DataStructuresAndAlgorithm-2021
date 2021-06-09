328e63959693c6e87a9722c9312206dd36805fc7
trudykush <kushalmishra91@gmail.com>
2021-06-09 22:54:48 +0100

    Power using recursion

--- src/June2021/Recursion/PowerUsingRecursion.java
+++ src/June2021/Recursion/PowerUsingRecursion.java
@@ -1,4 +1,20 @@
 package June2021.Recursion;
 
+import java.util.Scanner;
+
 public class PowerUsingRecursion {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int numberOfCases = sc.nextInt();
+        while (numberOfCases-- > 0) {
+            int value = sc.nextInt();
+            int power = sc.nextInt();
+
+            recursivePower(value, power);
+        }
+    }
+
+    private static void recursivePower(int value, int power) {
+
+    }
 }
