d46ce0afe7b56b937fab77baedfb2b0967b5e609
trudykush <kushalmishra91@gmail.com>
2021-06-12 00:27:48 +0100

    Array implementation of queue - Part2

--- src/June2021/Queue/ArrayImpleQueuePart2.java
+++ src/June2021/Queue/ArrayImpleQueuePart2.java
@@ -8,6 +8,31 @@ public class ArrayImpleQueuePart2 {
         int numberOfCases = sc.nextInt();
         while (numberOfCases-- > 0) {
             int q = sc.nextInt();
+            MyQueue obj = new MyQueue(q);
+            while (q-- > 0) {
+                int queueType = sc.nextInt();
+                if (queueType == 1) {
+                    int a = sc.nextInt();
+                    obj.enqueue(a);
+                } else if (queueType == 2) {
+                    obj.dequeue();
+                }
+            }
+            System.out.println();
+        }
+    }
+
+    private static class MyQueue {
+
+        public MyQueue(int q) {
+
+        }
+
+        public void enqueue(int a) {
+
+        }
+
+        public void dequeue() {
 
         }
     }
