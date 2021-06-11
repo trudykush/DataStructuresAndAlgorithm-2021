1c246f8e2f6f06c4be6d74c8cc8632afbde4d286
trudykush <kushalmishra91@gmail.com>
2021-06-11 23:04:08 +0100

    Array implementation of queue - Part2

--- src/June2021/Queue/ArrayImpleQueuePart2.java
+++ src/June2021/Queue/ArrayImpleQueuePart2.java
@@ -1,4 +1,14 @@
 package June2021.Queue;
 
+import java.util.Scanner;
+
 public class ArrayImpleQueuePart2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int numberOfCases = sc.nextInt();
+        while (numberOfCases-- > 0) {
+            int q = sc.nextInt();
+
+        }
+    }
 }
