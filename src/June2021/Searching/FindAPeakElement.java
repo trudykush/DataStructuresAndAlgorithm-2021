5900bf75bc74f7bd876441aada48173c532cb591
trudykush <kushalmishra91@gmail.com>
2021-06-14 21:56:35 +0100

    Finding a peak element

--- src/June2021/Searching/FindAPeakElement.java
+++ src/June2021/Searching/FindAPeakElement.java
@@ -5,6 +5,12 @@ import java.util.Scanner;
 public class FindAPeakElement {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        System.out.println("Enter number of cases: ");
         int numberOfCases = sc.nextInt();
+
+        while (numberOfCases-- > 0) {
+            System.out.println("Enter the size of array: ");
+            int n = sc.nextInt();
+        }
     }
 }
