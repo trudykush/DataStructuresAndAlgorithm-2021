5b6b7b5d4a126c235155a8fc6c719d5bbab72f8a
trudykush <kushalmishra91@gmail.com>
2021-06-14 21:55:34 +0100

    Finding a peak element

--- src/June2021/Searching/FindAPeakElement.java
+++ src/June2021/Searching/FindAPeakElement.java
@@ -1,4 +1,10 @@
 package June2021.Searching;
 
+import java.util.Scanner;
+
 public class FindAPeakElement {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int numberOfCases = sc.nextInt();
+    }
 }
