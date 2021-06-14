3f8dff9436ff3b31369b4a770add3780eb0bb6c2
trudykush <kushalmishra91@gmail.com>
2021-06-14 21:58:16 +0100

    Finding a peak element

--- src/June2021/Searching/FindAPeakElement.java
+++ src/June2021/Searching/FindAPeakElement.java
@@ -11,6 +11,18 @@ public class FindAPeakElement {
         while (numberOfCases-- > 0) {
             System.out.println("Enter the size of array: ");
             int n = sc.nextInt();
+
+            int[] arr = new int[n];
+            System.out.println("Enter the elements for array of " + n + " size.");
+            for (int i = 0; i < n; i++) {
+                arr[i] = sc.nextInt();
+            }
+
+            displayArray(arr, n);
         }
+    }
+
+    private static void displayArray(int[] arr, int n) {
+
     }
 }
