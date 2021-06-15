4bacd76c3e8bd7ba0ae01500e438edd4bffcc544
trudykush <kushalmishra91@gmail.com>
2021-06-15 21:40:19 +0100

    Minimum number in a sorted rotated array

--- src/June2021/Searching/MinimumNumberInSortedRotatedArray.java
+++ src/June2021/Searching/MinimumNumberInSortedRotatedArray.java
@@ -15,11 +15,26 @@ public class MinimumNumberInSortedRotatedArray {
                 arr[i] = sc.nextInt();
             }
 
-            minimumNumber(arr, n);
+            int result = minimumNumber(arr, n);
         }
     }
 
-    private static void minimumNumber(int[] arr, int n) {
+    private static int minimumNumber(int[] arr, int n) {
+        return minNumberRecursion(arr, 0, n-1, n);
+    }
 
+    private static int minNumberRecursion(int[] arr, int low, int high, int n) {
+
+        int mid = low + (high-low)/2;
+
+        if ((mid > 0 && arr[mid] < arr[mid-1]) ||
+                (mid < n-1 && arr[mid] < arr[mid+1])) {
+            return mid;
+        } else if (arr[mid] < low) {
+            minNumberRecursion(arr, low, mid-1, n);
+        } else {
+            minNumberRecursion(arr, mid+1, high, n);
+        }
+        return -1;
     }
 }
