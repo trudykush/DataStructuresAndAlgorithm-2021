f713f714d50654beda5093d8fcf3d6af8c917016
trudykush <kushalmishra91@gmail.com>
2021-06-15 21:30:06 +0100

    Minimum number in a sorted rotated array

--- src/June2021/Searching/MinimumNumberInSortedRotatedArray.java
+++ src/June2021/Searching/MinimumNumberInSortedRotatedArray.java
@@ -5,9 +5,21 @@ import java.util.Scanner;
 public class MinimumNumberInSortedRotatedArray {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        System.out.println("Number of cases: ");
         int numberOfCases = sc.nextInt();
         while (numberOfCases-- > 0) {
+            System.out.println("Size of array: ");
+            int n = sc.nextInt();
+            int[] arr = new int[n];
+            for (int i = 0; i < n; i++) {
+                arr[i] = sc.nextInt();
+            }
 
+            minimumNumber(arr, n);
         }
+    }
+
+    private static void minimumNumber(int[] arr, int n) {
+
     }
 }
