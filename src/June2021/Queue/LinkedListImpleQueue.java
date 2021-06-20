f24aa726271dd1aaa31a99a87b6890c44383f4e4
trudykush <kushalmishra91@gmail.com>
2021-06-20 16:57:50 +0100

    LinkedList Implementation of Queue

--- src/June2021/Queue/LinkedListImpleQueue.java
+++ src/June2021/Queue/LinkedListImpleQueue.java
@@ -5,9 +5,29 @@ import java.util.Scanner;
 public class LinkedListImpleQueue {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number of cases: ");
         int numberOfCases = sc.nextInt();
         while (numberOfCases-- > 0) {
+            System.out.println("Enter the size of queue: ");
+            int q = sc.nextInt();
 
+            MyLinkedListQueue queue = new MyLinkedListQueue(q);
+        }
+    }
+
+    private static class MyLinkedListQueue {
+
+        public MyLinkedListQueue(int q) {
+        }
+    }
+
+    private static class QNode {
+        int key;
+        QNode next;
+
+        public QNode(int key) {
+            this.key = key;
+            this.next = null;
         }
     }
 }
