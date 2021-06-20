8ff603a610effede8c0457bcf2ac69ebd91a2d95
trudykush <kushalmishra91@gmail.com>
2021-06-20 17:03:16 +0100

    LinkedList Implementation of Queue

--- src/June2021/Queue/LinkedListImpleQueue.java
+++ src/June2021/Queue/LinkedListImpleQueue.java
@@ -11,13 +11,41 @@ public class LinkedListImpleQueue {
             System.out.println("Enter the size of queue: ");
             int q = sc.nextInt();
 
-            MyLinkedListQueue queue = new MyLinkedListQueue(q);
+            MyLinkedListQueue queue = new MyLinkedListQueue();
+            while (q-- > 0) {
+                System.out.println("Enter 1 for Enqueue or 2 for Dequeue");
+                int queueType = sc.nextInt();
+                if (queueType == 1) {
+                    int a = sc.nextInt();
+                    queue.enqueue(a);
+                } else {
+                    queue.dequeue();
+                }
+            }
         }
     }
 
     private static class MyLinkedListQueue {
+        QNode front, rear;
 
-        public MyLinkedListQueue(int q) {
+        public MyLinkedListQueue() {
+            this.front = this.rear = null;
+        }
+
+        public void enqueue(int key) {
+
+            QNode temp = new QNode(key);
+            if (this.rear == null) {
+                this.front = this.rear = temp;
+                return;
+            }
+
+            this.rear.next = temp;
+            this.rear = temp;
+        }
+
+        public void dequeue() {
+
         }
     }
 
