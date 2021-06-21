2f657929e87928cabe6a0bdeaf1955b4ff46476d
trudykush <kushalmishra91@gmail.com>
2021-06-21 22:25:07 +0100

    Reverse Queue

---
+++ src/June2021/Queue/ReverseQueue.java
@@ -0,0 +1,20 @@
+package June2021.Queue;
+
+import java.util.Scanner;
+
+public class ReverseQueue {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Number Of Cases: ");
+        int numberOfCases = sc.nextInt();
+
+        while (numberOfCases -- > 0) {
+            System.out.println("Size of Queue");
+            int q = sc.nextInt();
+
+            while (q-- > 0) {
+
+            }
+        }
+    }
+}
