41995cda6c5d05adb3902d2342fcd0b8a18bdfbf
trudykush <kushalmishra91@gmail.com>
2021-06-23 21:53:53 +0100

    Implementing Queue using Stacks

---
+++ src/June2021/Queue/QueueUsingTwoStack.java
@@ -0,0 +1,13 @@
+package June2021.Queue;
+
+import java.util.Scanner;
+
+public class QueueUsingTwoStack {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int numberOfCases = sc.nextInt();
+        while (numberOfCases-- > 0) {
+
+        }
+    }
+}
