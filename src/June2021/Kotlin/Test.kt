8450f1ec254da75dc4d59aa0e9344b390de8f659
trudykush <kushalmishra91@gmail.com>
2021-06-24 22:26:34 +0100

    Kotlin practice

--- src/June2021/Kotlin/Test.kt
+++ src/June2021/Kotlin/Test.kt
@@ -4,16 +4,20 @@ fun main() {
     println("Hello World")
 
     var neverNull: String = "This can't be null"
-    neverNull = null
+    //neverNull = null
 
     var nullable: String? = "You can keep a null here"
     nullable = null
 
     var inferredNonNull = "The compiler assumes non-null"
-    inferredNonNull = null
+    //inferredNonNull = null
 
     val result = strLength(neverNull)
     println(result)
+
+    /* //Error
+    val resultNull = strLength(nullable)
+    println(resultNull)*/
 }
 
 fun strLength(nonNull: String) :Int {
