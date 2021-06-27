0aa328422721ae97c44d462cae44d4bca1d31c47
trudykush <kushalmishra91@gmail.com>
2021-06-27 21:39:23 +0100

    Control Flow in Kotlin

---
+++ src/June2021/Kotlin/KotlinEquality.kt
@@ -0,0 +1,12 @@
+package June2021.Kotlin
+
+fun main() {
+    val authors = setOf("Shakespeare", "Hemingway", "Twain")
+    val writers = setOf("Twain", "Shakespeare", "Hemingway")
+
+    // Returns true because it calls authors.equals(writes) and sets ignore element order
+    println(authors == writers)
+
+    // Returns false because authors and writes are distinct references
+    println(authors === writers)
+}
--- src/June2021/Kotlin/KotlinRanges.kt
+++ src/June2021/Kotlin/KotlinRanges.kt
@@ -42,6 +42,8 @@ fun main() {
         print("x is in range from 1 to 5")
     }
 
+    println()
+
     if (x !in 6..10) {
         print("x is not in range from 6 to 10")
     }
