085736b9e9226fef036740d5ad64b66b91351019
trudykush <kushalmishra91@gmail.com>
2021-06-27 21:20:47 +0100

    Control Flow in Kotlin

--- src/June2021/Kotlin/ControlFlow.kt
+++ src/June2021/Kotlin/ControlFlow.kt
@@ -4,18 +4,36 @@ import org.omg.CORBA.Object
 
 fun main() {
 
-    val cakes = listOf<String>("carrot", "Cheese", "Chocolate")
+    // while and do-while
+    var cakesEaten = 0
+    var cakesBakes = 0
 
+    while (cakesEaten < 5) {
+        eatACake()
+        cakesEaten++
+    }
+
+    do {
+        bakeACake()
+        cakesBakes++
+    } while (cakesBakes < cakesEaten)
+
+    // for loop
+    val cakes = listOf<String>("carrot", "Cheese", "Chocolate")
     for (cake in cakes) {
         println("Yummy, it's a $cake cake!")
     }
 
+    // when statements
     println(whenAssigned("Hello"))
     println(whenAssigned("hello"))
     println(whenAssigned(1))
     println(whenAssigned(31L))
     println(whenAssigned(ControlFlow()))
 }
+
+fun eatACake() = println("Eat a cake!")
+fun bakeACake() = println("Bake a cake!")
 
 fun whenAssigned(obj: Any): Any {
     val result = when (obj) {
