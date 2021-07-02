0e024ed2e7a89ff4ddf70a7f95c03c3e85439bc7
trudykush <kushalmishra91@gmail.com>
2021-07-02 23:29:29 +0100

    Bubble Sort

---
+++ src/July2021/Sorting/BubbleSort.java
@@ -0,0 +1,13 @@
+package July2021.Sorting;
+
+import java.util.Scanner;
+
+public class BubbleSort {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int numberOfCases = sc.nextInt();
+        while (numberOfCases-- > 0) {
+
+        }
+    }
+}
