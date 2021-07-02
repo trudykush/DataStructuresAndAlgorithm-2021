cf439b6d245d7d4acbd051a0006df00e68090eeb
trudykush <kushalmishra91@gmail.com>
2021-07-02 23:31:20 +0100

    Bubble Sort

--- src/July2021/Sorting/BubbleSort.java
+++ src/July2021/Sorting/BubbleSort.java
@@ -5,9 +5,21 @@ import java.util.Scanner;
 public class BubbleSort {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        System.out.println("Number of cases: ");
         int numberOfCases = sc.nextInt();
         while (numberOfCases-- > 0) {
+            System.out.println("Size of array");
+            int n = sc.nextInt();
+            int[] arr = new int[n];
+            for (int i = 0; i < n; i++) {
+                arr[i] = sc.nextInt();
+            }
 
+            bubbleSortSolution(arr, n);
         }
+    }
+
+    private static void bubbleSortSolution(int[] arr, int n) {
+
     }
 }
