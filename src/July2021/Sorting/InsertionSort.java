7f0959db93bd169b68766d4850a7a5e65cd17705
trudykush <kushalmishra91@gmail.com>
2021-07-03 14:57:32 +0100

    Insertion Sort

---
+++ src/July2021/Sorting/InsertionSort.java
@@ -0,0 +1,42 @@
+package July2021.Sorting;
+
+import java.util.Scanner;
+
+public class InsertionSort {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Number of Cases: ");
+        int numberOfCases = sc.nextInt();
+        while (numberOfCases-- > 0) {
+            System.out.println("Array length: ");
+            int n = sc.nextInt();
+            int[] arr = new int[n];
+            for (int i = 0; i < n; i++) {
+                arr[i] = sc.nextInt();
+            }
+            insertionSortSolution(arr, n);
+            printSolution(arr);
+        }
+    }
+
+    private static void printSolution(int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+    }
+
+    private static void insertionSortSolution(int[] arr, int n) {
+        int pointer = arr[0];
+        for (int i = 1; i < n; i++) {
+            if (arr[i] < arr[i-1]) {
+                int temp = arr[i];
+                arr[i] = arr[i-1];
+                arr[i-1] = temp;
+            }
+            pointer++;
+        }
+    }
+
+}
