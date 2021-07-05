532938888dcd879bdd9e58150c3b2e238429abb0
trudykush <kushalmishra91@gmail.com>
2021-07-05 23:34:56 +0100

    Quick Sort

---
+++ src/July2021/Sorting/QuickSort.java
@@ -0,0 +1,20 @@
+package July2021.Sorting;
+
+import java.util.Scanner;
+
+public class QuickSort {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter Number of cases: ");
+        int numberOfCases = sc.nextInt();
+        while (numberOfCases-- > 0) {
+            System.out.println("Size of array");
+            int n = sc.nextInt();
+            int[] arr = new int[n];
+            System.out.println("Enter Elements of array:");
+            for (int i = 0; i < n; i++) {
+                arr[i] = sc.nextInt();
+            }
+        }
+    }
+}
