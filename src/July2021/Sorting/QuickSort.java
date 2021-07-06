08226ed7ba546c95a1538220c90282abb4929680
trudykush <kushalmishra91@gmail.com>
2021-07-06 21:57:49 +0100

    Quick Sort

--- src/July2021/Sorting/QuickSort.java
+++ src/July2021/Sorting/QuickSort.java
@@ -65,6 +65,10 @@ public class QuickSort {
     }
 
     private static void swap(int[] arr, int i, int j) {
-
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
     }
+
+
 }
