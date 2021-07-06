92ee64e56d8ce9044963ffcf1614d8e6710c30c2
trudykush <kushalmishra91@gmail.com>
2021-07-06 20:58:49 +0100

    Quick Sort

--- src/July2021/Sorting/QuickSort.java
+++ src/July2021/Sorting/QuickSort.java
@@ -30,6 +30,11 @@ public class QuickSort {
         }
     }
 
+    /*
+    * This function takes last element as pivot, places the pivot element
+    * at its correct position in sorted array, and places all smaller (smaller than pivot)
+    * to left of pivot and all greater elements to right of pivot
+    * */
     private static int partition(int[] arr, int low, int high) {
 
         // pivot
@@ -43,6 +48,7 @@ public class QuickSort {
             // If current element if smaller
             // than the pivot
             if (arr[j] < pivot) {
+                // Increment index of smaller element
                 i++;
                 swap(arr, i, j);
             }
