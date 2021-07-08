604eb91df7ccd7436f0acdac4c31bb8bc449d776
trudykush <kushalmishra91@gmail.com>
2021-07-08 22:57:19 +0100

    Count inversion in array

--- src/July2021/Sorting/CountInversionInArray.java
+++ src/July2021/Sorting/CountInversionInArray.java
@@ -15,6 +15,20 @@ public class CountInversionInArray {
             for (int i = 0; i < n; i++) {
                 arr[i] = sc.nextInt();
             }
+
+            enhancedMergeSort(arr, n);
         }
+    }
+
+    /*
+    * To count the inversion in array - we will use the enhanced Merge sort
+    * Complexity Analysis:
+    * Time Complexity: O(n log n), The algorithm used is divide and conquer,
+    * So in each level, one full array traversal is needed,
+    * and there are log n levels, so the time complexity is O(n log n).
+    * Space Complexity: O(n), Temporary array.
+    * */
+    private static void enhancedMergeSort(int[] arr, int n) {
+
     }
 }
