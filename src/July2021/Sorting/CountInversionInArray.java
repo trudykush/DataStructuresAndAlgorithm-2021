65af306f481d841ca1e8e2f97e04f5478fe20799
trudykush <kushalmishra91@gmail.com>
2021-07-08 22:50:37 +0100

    Count inversion in array

--- src/July2021/Sorting/CountInversionInArray.java
+++ src/July2021/Sorting/CountInversionInArray.java
@@ -8,7 +8,13 @@ public class CountInversionInArray {
         System.out.println("Enter number of cases: ");
         int numberOfCases = sc.nextInt();
         while (numberOfCases-- > 0) {
-
+            System.out.println("Enter size of array: ");
+            int n = sc.nextInt();
+            int[] arr = new int[n];
+            System.out.println("Enter the elements of array: ");
+            for (int i = 0; i < n; i++) {
+                arr[i] = sc.nextInt();
+            }
         }
     }
 }
