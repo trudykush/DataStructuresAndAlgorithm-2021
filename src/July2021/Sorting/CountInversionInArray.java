0544547a1151712525da5f5ed79264cebfd2a9c1
trudykush <kushalmishra91@gmail.com>
2021-07-08 22:49:26 +0100

    Count inversion in array

---
+++ src/July2021/Sorting/CountInversionInArray.java
@@ -0,0 +1,14 @@
+package July2021.Sorting;
+
+import java.util.Scanner;
+
+public class CountInversionInArray {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter number of cases: ");
+        int numberOfCases = sc.nextInt();
+        while (numberOfCases-- > 0) {
+
+        }
+    }
+}
