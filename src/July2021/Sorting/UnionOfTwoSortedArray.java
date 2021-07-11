f75fb6b97eca85c6f06844b77447f06f45877bdc
trudykush <kushalmishra91@gmail.com>
2021-07-11 11:26:19 +0100

    Union of two sorted array

--- src/July2021/Sorting/UnionOfTwoSortedArray.java
+++ src/July2021/Sorting/UnionOfTwoSortedArray.java
@@ -6,9 +6,16 @@ import java.util.Scanner;
 public class UnionOfTwoSortedArray {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number of cases: ");
         int numberOfCases = sc.nextInt();
         while (numberOfCases-- > 0) {
-
+            System.out.println("Enter the length of first array: ");
+            int n = sc.nextInt();
+            System.out.println("Enter the elements of the first array: ");
+            int[] arr1 = new int[n];
+            for (int i = 0; i < n; i++) {
+                arr1[i] = sc.nextInt();
+            }
         }
     }
 }
