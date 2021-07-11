fd84c400e9e315e4a04153e3c2f2b00ee099345a
trudykush <kushalmishra91@gmail.com>
2021-07-11 11:24:52 +0100

    Union of two sorted array

---
+++ src/July2021/Sorting/UnionOfTwoSortedArray.java
@@ -0,0 +1,14 @@
+package July2021.Sorting;
+
+import java.util.Scanner;
+
+// Include distinct elements in the final sorted array
+public class UnionOfTwoSortedArray {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int numberOfCases = sc.nextInt();
+        while (numberOfCases-- > 0) {
+
+        }
+    }
+}
