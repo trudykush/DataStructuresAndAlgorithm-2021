654ef9598ad44560101b991238df3fce13eb7011
trudykush <kushalmishra91@gmail.com>
2021-07-13 22:19:23 +0100

    Intersection of two sorted array

---
+++ src/July2021/Sorting/IntersectionOfTwoSortedArrays.java
@@ -0,0 +1,14 @@
+package July2021.Sorting;
+
+import java.util.Scanner;
+
+public class IntersectionOfTwoSortedArrays {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number of test cases: ");
+        int n = sc.nextInt();
+        while (n-- > 0) {
+
+        }
+    }
+}
