95e601a5ab6ba2eac5dc8874fe919e34a83d5b1d
trudykush <kushalmishra91@gmail.com>
2021-07-13 22:23:10 +0100

    Intersection of two sorted array

--- src/July2021/Sorting/IntersectionOfTwoSortedArrays.java
+++ src/July2021/Sorting/IntersectionOfTwoSortedArrays.java
@@ -8,12 +8,27 @@ public class IntersectionOfTwoSortedArrays {
         System.out.println("Enter the number of test cases: ");
         int n = sc.nextInt();
         while (n-- > 0) {
-            System.out.println("Enter the size of first array");
+            System.out.println("Enter the size of first array: ");
             int m1 = sc.nextInt();
             System.out.println("Enter the elements of first array: ");
+            int[] arr1 = new int[m1];
             for (int i = 0; i < m1; i++) {
-
+                arr1[i] = sc.nextInt();
             }
+
+            System.out.println("Enter the size of second array: ");
+            int m2 = sc.nextInt();
+            System.out.println("Enter the elements of second array: ");
+            int[] arr2 = new int[m2];
+            for (int i = 0; i < m2; i++) {
+                arr2[i] = sc.nextInt();
+            }
+
+            intersectionSolution(arr1, m1, arr2, m2);
         }
+    }
+
+    private static void intersectionSolution(int[] arr1, int m1, int[] arr2, int m2) {
+
     }
 }
