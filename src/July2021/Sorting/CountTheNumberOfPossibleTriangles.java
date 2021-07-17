f6d8885a4e3a705d0be0b1e8fd563a5a4df7d1ba
trudykush <kushalmishra91@gmail.com>
2021-07-17 23:41:37 +0100

    Count the number of possible triangles

--- src/July2021/Sorting/CountTheNumberOfPossibleTriangles.java
+++ src/July2021/Sorting/CountTheNumberOfPossibleTriangles.java
@@ -1,5 +1,7 @@
 package July2021.Sorting;
 
+import java.util.Scanner;
+
 /*
 * For a triangle to be possible from 3 values,
 * the sum of any of the two values (or sides)
@@ -7,6 +9,11 @@ package July2021.Sorting;
  * */
 public class CountTheNumberOfPossibleTriangles {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number of cases: ");
+        int n = sc.nextInt();
+        while (n-- > 0) {
 
+        }
     }
 }
