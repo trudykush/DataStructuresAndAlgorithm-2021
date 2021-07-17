105db5a5f4b0034c35f2c2c8adbd8f61e9357431
trudykush <kushalmishra91@gmail.com>
2021-07-17 23:43:08 +0100

    Count the number of possible triangles

--- src/July2021/Sorting/CountTheNumberOfPossibleTriangles.java
+++ src/July2021/Sorting/CountTheNumberOfPossibleTriangles.java
@@ -11,9 +11,14 @@ public class CountTheNumberOfPossibleTriangles {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter the number of cases: ");
-        int n = sc.nextInt();
-        while (n-- > 0) {
-
+        int m = sc.nextInt();
+        while (m-- > 0) {
+            int n = sc.nextInt();
+            System.out.println("Enter the size of array: ");
+            int[] arr = new int[n];
+            for (int i = 0; i < n; i++) {
+                arr[i] = sc.nextInt();
+            }
         }
     }
 }
