a1f0a649be1a3cc85a3e04d7056b8752959b9195
trudykush <kushalmishra91@gmail.com>
2021-07-17 23:40:47 +0100

    Count the number of possible triangles

--- src/July2021/Sorting/CountTheNumberOfPossibleTriangles.java
+++ src/July2021/Sorting/CountTheNumberOfPossibleTriangles.java
@@ -1,5 +1,10 @@
 package July2021.Sorting;
 
+/*
+* For a triangle to be possible from 3 values,
+* the sum of any of the two values (or sides)
+* must be greater than the third value (or third side).
+ * */
 public class CountTheNumberOfPossibleTriangles {
     public static void main(String[] args) {
 
