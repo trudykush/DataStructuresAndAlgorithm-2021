d53038d4f3ce993f9dce8f066a1b242907990f60
trudykush <kushalmishra91@gmail.com>
2021-07-20 22:03:29 +0100

    sorting 0s, 1s and 2s

--- src/July2021/Sorting/Sort012.java
+++ src/July2021/Sorting/Sort012.java
@@ -1,7 +1,14 @@
 package July2021.Sorting;
 
+import java.util.Scanner;
+
 public class Sort012 {
     public static void main(String[] args) {
-
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number of cases: ");
+        int numberOfCases = sc.nextInt();
+        while (numberOfCases-- > 0) {
+            System.out.println();
+        }
     }
 }
