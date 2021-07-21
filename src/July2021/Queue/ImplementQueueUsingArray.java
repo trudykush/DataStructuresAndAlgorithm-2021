34533eee2bc855b914b9ee27662c8bc3363c4ff5
trudykush <kushalmishra91@gmail.com>
2021-07-21 23:17:12 +0100

    Implementing queue using array

---
+++ src/July2021/Queue/ImplementQueueUsingArray.java
@@ -0,0 +1,14 @@
+package July2021.Queue;
+
+import java.util.Scanner;
+
+public class ImplementQueueUsingArray {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number of cases: ");
+        int numberOfCases = sc.nextInt();
+        while (numberOfCases-- > 0) {
+
+        }
+    }
+}
