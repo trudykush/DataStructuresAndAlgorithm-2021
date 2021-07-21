a7d8809f7226967a05c6af6bb09bd6fb17de1878
trudykush <kushalmishra91@gmail.com>
2021-07-21 22:50:07 +0100

    sorting 0s, 1s and 2s

--- src/July2021/Sorting/Sort012.java
+++ src/July2021/Sorting/Sort012.java
@@ -22,8 +22,9 @@ public class Sort012 {
 
     private static void print(int[] arr) {
         for (int x : arr) {
-            System.out.println(x+ " ");
+            System.out.print(x+ " ");
         }
+        System.out.println();
     }
 
     private static void sorting012(int[] arr, int n) {
