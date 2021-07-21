310ae99b8e2d9f46e3ad40f0529f668fa36275cc
trudykush <kushalmishra91@gmail.com>
2021-07-21 23:23:35 +0100

    Implementing queue using array

--- src/July2021/Queue/ImplementQueueUsingArray.java
+++ src/July2021/Queue/ImplementQueueUsingArray.java
@@ -10,14 +10,32 @@ public class ImplementQueueUsingArray {
         while (numberOfCases-- > 0) {
             System.out.println("Enter the size of queue: ");
             int q = sc.nextInt();
-            int[] queueArray = new int[q];
+            Solution obj = new Solution(q);
             System.out.println("Enter 1 to enQueue, 2 to deQueue");
             int condition = sc.nextInt();
             if (condition == 1) {
                 // enQueue
+                System.out.println("Enter number to enQueue: ");
+                int element = sc.nextInt();
+                obj.enQueue(element);
             } else {
                 // deQueue
+                obj.deQueue();
             }
+        }
+    }
+
+    private static class Solution {
+        public Solution(int q) {
+
+        }
+
+        public void enQueue(int element) {
+
+        }
+
+        public void deQueue() {
+
         }
     }
 }
