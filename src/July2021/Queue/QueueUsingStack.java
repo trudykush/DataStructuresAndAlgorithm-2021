279d68c8b53cd14a286a9da24f955e771272c4ee
trudykush <kushalmishra91@gmail.com>
2021-07-25 22:24:38 +0100

    Queue using Stack

---
+++ src/July2021/Queue/QueueUsingStack.java
@@ -0,0 +1,14 @@
+package July2021.Queue;
+
+import java.util.Scanner;
+
+public class QueueUsingStack {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number of cases: ");
+        int numOfCases = sc.nextInt();
+        while (numOfCases-- > 0) {
+
+        }
+    }
+}
