ff0c711c60433a662de06d5c8b463a3abd98d775
trudykush <kushalmishra91@gmail.com>
2021-07-25 22:28:20 +0100

    Queue using Stack

--- src/July2021/Queue/QueueUsingStack.java
+++ src/July2021/Queue/QueueUsingStack.java
@@ -8,7 +8,18 @@ public class QueueUsingStack {
         System.out.println("Enter the number of cases: ");
         int numOfCases = sc.nextInt();
         while (numOfCases-- > 0) {
-
+            System.out.println("Enter the size of Queue: ");
+            int n = sc.nextInt();
+            QueueStack qStack = new QueueStack();
+            System.out.println("Enter choice of action: ");
+            int qChoice = sc.nextInt();
+            if (qChoice == 1) {
+                System.out.println("Enter the element: ");
+//                qStack.enqueue();
+            }
         }
+    }
+
+    private static class QueueStack {
     }
 }
