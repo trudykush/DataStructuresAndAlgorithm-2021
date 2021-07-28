0890985e7ec7d565179e30b141b71396db7c6847
trudykush <kushalmishra91@gmail.com>
2021-07-28 22:05:57 +0100

    PreOrder Tree

--- src/July2021/Trees/PreOrder.java
+++ src/July2021/Trees/PreOrder.java
@@ -2,6 +2,14 @@ package July2021.Trees;
 
 public class PreOrder {
     public static void main(String[] args) {
-
+        BinaryTree tree = new BinaryTree();
+        tree.root = new Node(1);
+        tree.root.left = new Node(2);
+        tree.root.right = new Node(3);
+        tree.root.left.left = new Node(4);
+        tree.root.left.right = new Node(5);
     }
 }
+
+
+class Node {}
