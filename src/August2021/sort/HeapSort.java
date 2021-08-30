f49cabf0b915847b3724816dc61c37d6d5b85b0e
trudykush <kushalmishra91@gmail.com>
2021-08-30 23:32:14 +0100

    heap sort

--- src/August2021/sort/HeapSort.java
+++ src/August2021/sort/HeapSort.java
@@ -5,7 +5,11 @@ import java.util.Scanner;
 public class HeapSort {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        System.out.println("Number of test cases: ");
         int numberOfCases = sc.nextInt();
-
+        while (numberOfCases-- > 0) {
+            System.out.println("Enter size of array: ");
+            int n = sc.nextInt();
+        }
     }
 }
