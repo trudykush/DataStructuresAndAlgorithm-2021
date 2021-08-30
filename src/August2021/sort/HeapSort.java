03ede829a49ecd332b983a8a7de04d8013d280fe
trudykush <kushalmishra91@gmail.com>
2021-08-30 23:30:55 +0100

    heap sort

--- src/August2021/sort/HeapSort.java
+++ src/August2021/sort/HeapSort.java
@@ -1,4 +1,11 @@
 package August2021.sort;
 
+import java.util.Scanner;
+
 public class HeapSort {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int numberOfCases = sc.nextInt();
+
+    }
 }
