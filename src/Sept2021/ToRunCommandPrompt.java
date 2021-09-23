34b0929ddc722fca2d80986caf945482254c5447
trudykush <kushalmishra91@gmail.com>
2021-09-23 22:31:46 +0100

    running cmd prompt form code

---
+++ src/Sept2021/ToRunCommandPrompt.java
@@ -0,0 +1,24 @@
+package Sept2021;
+
+import java.io.IOException;
+
+public class ToRunCommandPrompt {
+    public static void main(String[] args) {
+
+        String command = "cmd.exe /c start "+"cmd.exe";
+        try {
+            Process child = Runtime.getRuntime().exec(command);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        Runtime rt = Runtime.getRuntime();
+        String new_dir = "C://";
+        try {
+            rt.exec("cmd.exe /c cd \""+new_dir+"\" & start cmd.exe javac ");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+}
