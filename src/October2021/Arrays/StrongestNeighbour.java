f02a529f06ff39c2a7e7ad3c9515fdfb6224457c
trudykush <kushalmishra91@gmail.com>
2021-10-13 22:55:29 +0100

    StrongestNeighbour

--- src/October2021/Arrays/StrongestNeighbour.java
+++ src/October2021/Arrays/StrongestNeighbour.java
@@ -5,9 +5,14 @@ import java.util.Scanner;
 public class StrongestNeighbour {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+
         int numberOfCases = sc.nextInt();
         while (numberOfCases-- > 0) {
-
+            int sizeOfArray = sc.nextInt();
+            int[] arr = new int[sizeOfArray];
+            for (int i = 0; i < arr.length; i++) {
+                arr[i] = sc.nextInt();
+            }
         }
     }
 }
