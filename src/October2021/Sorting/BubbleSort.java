adc40b23a59e99d70300637bd67ab64b305511c9
trudykush <kushalmishra91@gmail.com>
2021-10-14 00:02:52 +0100

    BubbleSort

--- src/October2021/Sorting/BubbleSort.java
+++ src/October2021/Sorting/BubbleSort.java
@@ -23,7 +23,18 @@ public class BubbleSort {
         boolean swapped;
 
         for (i = 0; i < n - 1; i++) {
-
+            swapped = false;
+            for (j = 0; j < n - i - 1; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    temp = arr[j];
+                    arr[j] = arr[j+1];
+                    arr[j+1] = temp;
+                    swapped = true;
+                }
+            }
+            if (!swapped) {
+                break;
+            }
         }
     }
 }
