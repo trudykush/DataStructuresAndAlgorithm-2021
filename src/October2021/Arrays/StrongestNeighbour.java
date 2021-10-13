ea6c97346d94e839b28cf50a7027fc88ef9b34d9
trudykush <kushalmishra91@gmail.com>
2021-10-13 22:58:30 +0100

    StrongestNeighbour

--- src/October2021/Arrays/StrongestNeighbour.java
+++ src/October2021/Arrays/StrongestNeighbour.java
@@ -5,14 +5,21 @@ import java.util.Scanner;
 public class StrongestNeighbour {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-
+        System.out.println("Enter the number of cases: ");
         int numberOfCases = sc.nextInt();
         while (numberOfCases-- > 0) {
+            System.out.println("Enter the size of the array: ");
             int sizeOfArray = sc.nextInt();
             int[] arr = new int[sizeOfArray];
+            System.out.println("Enter the elements of the array: ");
             for (int i = 0; i < arr.length; i++) {
                 arr[i] = sc.nextInt();
             }
+            maximumAdjacent(arr, sizeOfArray);
         }
+    }
+
+    private static void maximumAdjacent(int[] arr, int n) {
+
     }
 }
