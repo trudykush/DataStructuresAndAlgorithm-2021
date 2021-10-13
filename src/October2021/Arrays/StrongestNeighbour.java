194d8b40fcc01f2cfd2fc2b3e945a4348c8d0d9f
trudykush <kushalmishra91@gmail.com>
2021-10-13 22:54:22 +0100

    StrongestNeighbour

--- src/October2021/Arrays/StrongestNeighbour.java
+++ src/October2021/Arrays/StrongestNeighbour.java
@@ -1,7 +1,13 @@
 package October2021.Arrays;
 
+import java.util.Scanner;
+
 public class StrongestNeighbour {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int numberOfCases = sc.nextInt();
+        while (numberOfCases-- > 0) {
 
+        }
     }
 }
