2f9615b778caee2bfe34af7af88fb827e2c5e670
trudykush <kushalmishra91@gmail.com>
2021-10-13 23:04:18 +0100

    StrongestNeighbour

--- src/October2021/Arrays/StrongestNeighbour.java
+++ src/October2021/Arrays/StrongestNeighbour.java
@@ -21,7 +21,10 @@ public class StrongestNeighbour {
     }
 
     private static void printResult(int[] result) {
-
+        for (int i = 0; i < result.length; i++) {
+            System.out.print(result[i] + " ");
+        }
+        System.out.println();
     }
 
     private static int[] maximumAdjacent(int[] arr, int n) {
