da68d5d7b329b77edb1de08e51faebe8f72c38ae
trudykush <kushalmishra91@gmail.com>
2021-10-17 22:42:32 +0100

    InsertionSort

--- src/October2021/Sorting/InsertionSort.java
+++ src/October2021/Sorting/InsertionSort.java
@@ -7,7 +7,11 @@ public class InsertionSort {
         Scanner sc = new Scanner(System.in);
         int numberOfCases = sc.nextInt();
         while (numberOfCases-- > 0) {
-
+            int n = sc.nextInt();
+            int[] arr = new int[n];
+            for (int i = 0; i < n; i++) {
+                arr[i] = sc.nextInt();
+            }
         }
     }
 }
