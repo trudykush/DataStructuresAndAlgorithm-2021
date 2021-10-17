e9c2a204eccc3560422ea59979ca92225b6deadc
trudykush <kushalmishra91@gmail.com>
2021-10-17 22:46:46 +0100

    InsertionSort

--- src/October2021/Sorting/InsertionSort.java
+++ src/October2021/Sorting/InsertionSort.java
@@ -18,6 +18,15 @@ public class InsertionSort {
     }
 
     private static void solutionInsertionSort(int[] arr, int n) {
+        for (int i = 1; i < n; i++) {
+            int key = arr[i];
+            int j = i - 1;
 
+            while (j >= 0 && arr[j] > key) {
+                arr[j+1] = arr[j];
+                j = j - 1;
+            }
+            arr[j+1] = key;
+        }
     }
 }
