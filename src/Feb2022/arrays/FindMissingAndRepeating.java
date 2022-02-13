bd4d573ded8f199b3ef653d9e32e797472bd1f74
trudykush <kushalmishra91@gmail.com>
2022-02-13 22:08:14 +0000

    FindMissingAndRepeating

--- src/Feb2022/arrays/FindMissingAndRepeating.java
+++ src/Feb2022/arrays/FindMissingAndRepeating.java
@@ -1,5 +1,19 @@
 package Feb2022.arrays;
 
-public class FindMissingAndRepeating {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
+public class FindMissingAndRepeating {
+    public static void main(String[] args) {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        try {
+            int numberOfCases = Integer.parseInt(br.readLine().trim());
+            while (numberOfCases-- > 0) {
+
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
