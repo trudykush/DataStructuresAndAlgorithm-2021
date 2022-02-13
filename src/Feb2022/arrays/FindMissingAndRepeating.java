a95f21d940d5f0e3f8acbb724a925e8943ce0244
trudykush <kushalmishra91@gmail.com>
2022-02-13 22:10:55 +0000

    FindMissingAndRepeating

--- src/Feb2022/arrays/FindMissingAndRepeating.java
+++ src/Feb2022/arrays/FindMissingAndRepeating.java
@@ -10,10 +10,24 @@ public class FindMissingAndRepeating {
         try {
             int numberOfCases = Integer.parseInt(br.readLine().trim());
             while (numberOfCases-- > 0) {
+                int n = Integer.parseInt(br.readLine().trim());
+                String[] str = br.readLine().split(" ");
 
+                int[] arr = new int[n];
+                for (int i = 0; i < n; i++) {
+                    arr[i] = Integer.parseInt(str[i]);
+                }
+
+                int[] ans = findTwoElements(arr, n);
+                System.out.println(ans[0] + " " + ans[1]);
             }
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
+
+    private static int[] findTwoElements(int[] arr, int n) {
+
+        return new int[0];
     }
 }
