c8c36f367e70bba237d4dc9b55debeebccae79eb
trudykush <kushalmishra91@gmail.com>
2022-02-14 15:05:41 +0000

    RotateArray

---
+++ src/Feb2022/arrays/RotateArray.java
@@ -0,0 +1,14 @@
+package Feb2022.arrays;
+
+import java.util.Scanner;
+
+public class RotateArray {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int numberOfCases = sc.nextInt();
+        while (numberOfCases-- > 0) {
+            int n = sc.nextInt();
+            int[] arr = new int[n];
+        }
+    }
+}
