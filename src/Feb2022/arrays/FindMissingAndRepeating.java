3af71b16b962e460e1f5a3d1db28dfee040c5f7f
trudykush <kushalmishra91@gmail.com>
2022-02-14 14:58:17 +0000

    FindMissingAndRepeating

--- src/Feb2022/arrays/FindMissingAndRepeating.java
+++ src/Feb2022/arrays/FindMissingAndRepeating.java
@@ -3,6 +3,10 @@ package Feb2022.arrays;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.lang.reflect.Array;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
 public class FindMissingAndRepeating {
     public static void main(String[] args) {
@@ -10,7 +14,7 @@ public class FindMissingAndRepeating {
         try {
             int numberOfCases = Integer.parseInt(br.readLine().trim());
             while (numberOfCases-- > 0) {
-                int n = Integer.parseInt(br.readLine().trim());
+                /*int n = Integer.parseInt(br.readLine().trim());
                 String[] str = br.readLine().split(" ");
 
                 int[] arr = new int[n];
@@ -19,29 +23,58 @@ public class FindMissingAndRepeating {
                 }
 
                 int[] ans = findTwoElements(arr, n);
-                System.out.println(ans[0] + " " + ans[1]);
+                System.out.println(ans[0] + " " + ans[1]);*/
+
+                hashMapSolution();
             }
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
 
-    private static int[] findTwoElements(int[] arr, int n) {
-        int repeat = 0;
-        int missing = 0;
-        int count = 1;
+    private static void hashMapSolution() {
+        int[] arr = { 4, 3, 6, 2, 1, 1 };
 
-        for (int i = 0; i < n - 1; i++) {
-            if (arr[i] == count) {
-                count++;
-                continue;
-            } else if (arr[i] == arr[i+1]) {
-                repeat = arr[i];
+        Map<Integer, Boolean> numberMap = new HashMap<>();
+
+        int n = arr.length;
+        for (Integer i : arr) {
+            if (numberMap.get(i) == null) {
+                numberMap.put(i, true);
             } else {
-                missing = count;
+                System.out.println("Repeating Number: " + i);
             }
         }
-        System.out.println(repeat + " " + missing);
-        return new int[0];
+
+        for (int i = 1; i <= n; i++) {
+            if (numberMap.get(i) == null) {
+                System.out.println("Missing Number: " + i);
+            }
+        }
+
+    }
+
+    private static int[] findTwoElements(int[] arr, int n) {
+        int repeat = 0;
+        int missing = 1;
+        int count = 2;
+        Arrays.sort(arr);
+        int[] result = new int[n];
+
+        for (int i = 1; i < n; i++) {
+            if (arr[i] != count) {
+                System.out.println(arr[i] != count);
+                System.out.println(arr[i] + " " + count);
+                missing = count;
+                count++;
+            } else if (arr[i] == arr[i - 1]) {
+                repeat = arr[i];
+                i++;
+            }
+        }
+        result[0] = repeat;
+        result[1] = missing;
+
+        return result;
     }
 }
