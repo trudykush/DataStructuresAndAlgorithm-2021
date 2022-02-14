96c779073eb31b19d6ea024034b8d41ed45a69dc
trudykush <kushalmishra91@gmail.com>
2022-02-14 23:32:41 +0000

    Wave array

--- src/Feb2022/arrays/WaveArray.java
+++ src/Feb2022/arrays/WaveArray.java
@@ -18,6 +18,36 @@ public class WaveArray {
     }
 
     private static void waveArraySolution(int[] arr, int n) {
+        simpleSolution(arr, n);
 
+        anotherSwapSolution(arr, n);
+    }
+
+    private static void anotherSwapSolution(int[] arr, int n) {
+        for (int i = 0; i < n; i++) {
+            // if current even element is smaller than previous - swap
+            if (i > 0 && arr[i-1] > arr[i]) {
+                swap(arr, i-1, i);
+            }
+
+            // if current even element is smaller than next - swap
+            if (i < n-1 && arr[i] < arr[i+1]) {
+                swap(arr, i, i+1);
+            }
+        }
+    }
+
+    private static void swap(int[] arr, int a, int b) {
+        int temp = arr[a];
+        arr[a] = arr[b];
+        arr[b] = temp;
+    }
+
+    private static void simpleSolution(int[] arr, int n) {
+        for (int i = 0; i < n-1; i++) {
+            int temp = arr[i];
+            arr[i] = arr[i+1];
+            arr[i+1] = temp;
+        }
     }
 }
