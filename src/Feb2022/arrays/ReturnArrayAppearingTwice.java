e73b43f2d7229efbd8bc4e1ffac72950e37d5d75
trudykush <kushalmishra91@gmail.com>
2022-02-21 22:46:56 +0000

    Appears twice

---
+++ src/Feb2022/arrays/ReturnArrayAppearingTwice.java
@@ -0,0 +1,17 @@
+package Feb2022.arrays;
+
+import java.util.Arrays;
+
+public class ReturnArrayAppearingTwice {
+    public static void main(String[] args) {
+        int[] arr = {8, 3, 2, 7, 3, 5, 8};
+
+        int[] result = appearsTwice(arr);
+        System.out.println(Arrays.toString(result));
+    }
+
+    private static int[] appearsTwice(int[] arr) {
+
+        return new int[0];
+    }
+}
