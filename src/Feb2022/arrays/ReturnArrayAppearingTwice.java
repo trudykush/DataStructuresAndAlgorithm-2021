bb2ac65c2f5cc195e782c88fd7e24d70abd7ae0e
trudykush <kushalmishra91@gmail.com>
2022-02-22 22:13:42 +0000

    Appears twice

--- src/Feb2022/arrays/ReturnArrayAppearingTwice.java
+++ src/Feb2022/arrays/ReturnArrayAppearingTwice.java
@@ -1,17 +1,29 @@
 package Feb2022.arrays;
 
-import java.util.Arrays;
+import java.util.*;
 
 public class ReturnArrayAppearingTwice {
     public static void main(String[] args) {
-        int[] arr = {8, 3, 2, 7, 3, 5, 8};
+        Integer[] arr = {8, 3, 2, 7, 3, 5, 8};
 
-        int[] result = appearsTwice(arr);
-        System.out.println(Arrays.toString(result));
+        Vector<Integer> result = appearsTwice(arr);
+        System.out.println(Arrays.toString(new Vector[]{result}));
     }
 
-    private static int[] appearsTwice(int[] arr) {
+    private static Vector<Integer> appearsTwice(Integer[] arr) {
 
-        return new int[0];
+        Set<Integer> integerSet = new HashSet<>();
+        Vector<Integer> vectorArr = new Vector<>(Arrays.asList(arr));
+        Collections.fill(vectorArr, 0);
+        int count = 0;
+        for (int j : arr) {
+            if (integerSet.contains(j)) {
+                vectorArr.set(count, j);
+                count++;
+            } else {
+                integerSet.add(j);
+            }
+        }
+        return vectorArr;
     }
 }
