12ed2ebe61f08dcc6006d1d5836492681e6f0217
trudykush <kushalmishra91@gmail.com>
2022-02-23 22:55:07 +0000

    InsertionSort

--- src/Feb2022/sorting/InsertionSort.java
+++ src/Feb2022/sorting/InsertionSort.java
@@ -1,7 +1,23 @@
 package Feb2022.sorting;
 
+import java.util.Scanner;
+
 public class InsertionSort {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int numberOfCases = sc.nextInt();
+        while (numberOfCases-- > 0) {
+            int n = sc.nextInt();
+            int[] arr = new int[n];
+            for (int i = 0; i < n; i++) {
+                arr[i] = sc.nextInt();
+            }
+
+            solutionInsertionSort(arr, n);
+        }
+    }
+
+    private static void solutionInsertionSort(int[] arr, int n) {
 
     }
 }
