c64adcc3e85540fffb0758864742b2ba20926caa
trudykush <kushalmishra91@gmail.com>
2022-02-23 22:58:35 +0000

    InsertionSort

--- src/Feb2022/sorting/InsertionSort.java
+++ src/Feb2022/sorting/InsertionSort.java
@@ -14,10 +14,22 @@ public class InsertionSort {
             }
 
             solutionInsertionSort(arr, n);
+
         }
     }
 
     private static void solutionInsertionSort(int[] arr, int n) {
+        int key;
+        int j;
+        for (int i = 1; i < n; i++) {
+            key = arr[i];
+            j = i - 1;
 
+            while (j >= 0 && arr[j] > key) {
+                arr[j+1] = arr[j];
+                j = j - 1;
+            }
+            arr[j+1] = key;
+        }
     }
 }
